26fd12a81cb83a75552d570c823f82c3d67602b3
Manuel de Brito Fontes <aledbf@gmail.com>
2016-06-23 10:38:08 -0400

    Avoid sync without a reachable master

--- controllers/nginx/controller.go
+++ controllers/nginx/controller.go
@@ -420,13 +420,17 @@ func (lbc *loadBalancerController) sync(key string) error {
 		return fmt.Errorf("deferring sync till endpoints controller has synced")
 	}
 
-	var cfg *api.ConfigMap
+	// by default no custom configuration configmap
+	cfg := &api.ConfigMap{}
 
-	ns, name, _ := parseNsName(lbc.nxgConfigMap)
-	cfg, err := lbc.getConfigMap(ns, name)
-	if err != nil {
-		glog.V(3).Infof("unexpected error searching configmap %v: %v", lbc.nxgConfigMap, err)
-		cfg = &api.ConfigMap{}
+	if lbc.nxgConfigMap != "" {
+		// Search for custom configmap (defined in main args)
+		var err error
+		ns, name, _ := parseNsName(lbc.nxgConfigMap)
+		cfg, err = lbc.getConfigMap(ns, name)
+		if err != nil {
+			return fmt.Errorf("unexpected error searching configmap %v: %v", lbc.nxgConfigMap, err)
+		}
 	}
 
 	ngxConfig := lbc.nginx.ReadConfig(cfg)
