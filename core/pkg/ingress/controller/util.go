ad247847005f26083c4213e9040ab1dfd20c5efb
Manuel de Brito Fontes <aledbf@gmail.com>
2017-03-09 19:08:26 -0300

    Only update Ingress status for the configured class

--- core/pkg/ingress/controller/util.go
+++ core/pkg/ingress/controller/util.go
@@ -22,11 +22,7 @@ import (
 	"github.com/golang/glog"
 	"github.com/imdario/mergo"
 
-	"k8s.io/kubernetes/pkg/apis/extensions"
-
 	"k8s.io/ingress/core/pkg/ingress"
-	"k8s.io/ingress/core/pkg/ingress/annotations/parser"
-	"k8s.io/ingress/core/pkg/ingress/errors"
 )
 
 // DeniedKeyName name of the key that contains the reason to deny a location
@@ -83,31 +79,6 @@ func matchHostnames(pattern, host string) bool {
 	}
 
 	return true
-}
-
-// IsValidClass returns true if the given Ingress either doesn't specify
-// the ingress.class annotation, or it's set to the configured in the
-// ingress controller.
-func IsValidClass(ing *extensions.Ingress, config *Configuration) bool {
-	currentIngClass := config.IngressClass
-
-	cc, err := parser.GetStringAnnotation(ingressClassKey, ing)
-	if err != nil && !errors.IsMissingAnnotations(err) {
-		glog.Warningf("unexpected error reading ingress annotation: %v", err)
-	}
-
-	// we have 2 valid combinations
-	// 1 - ingress with default class | blank annotation on ingress
-	// 2 - ingress with specific class | same annotation on ingress
-	//
-	// and 2 invalid combinations
-	// 3 - ingress with default class | fixed annotation on ingress
-	// 4 - ingress with specific class | different annotation on ingress
-	if (cc == "" && currentIngClass == "") || (currentIngClass == config.DefaultIngressClass) {
-		return true
-	}
-
-	return cc == currentIngClass
 }
 
 func mergeLocationAnnotations(loc *ingress.Location, anns map[string]interface{}) {
