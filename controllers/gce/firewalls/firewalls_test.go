49b780bca3328e112703654e66fa35aa05c9d936
Nick Sardo <nicksardo@google.com>
2017-04-11 08:32:52 -0700

    review comments

--- controllers/gce/firewalls/firewalls_test.go
+++ controllers/gce/firewalls/firewalls_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2015 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package firewalls
 
 import (
@@ -8,6 +24,8 @@ import (
 	"k8s.io/ingress/controllers/gce/utils"
 	netset "k8s.io/kubernetes/pkg/util/net/sets"
 )
+
+const allCIDR = "0.0.0.0/0"
 
 func TestSyncFirewallPool(t *testing.T) {
 	namer := utils.NewNamer("ABC", "XYZ")
@@ -32,13 +50,12 @@ func TestSyncFirewallPool(t *testing.T) {
 	}
 	verifyFirewallRule(fwp, ruleName, nodePorts, nodes, l7SrcRanges, t)
 
-	all := "0.0.0.0/0"
-	srcRanges, _ := netset.ParseIPNets(all)
+	srcRanges, _ := netset.ParseIPNets(allCIDR)
 	err = fwp.UpdateFirewall(namer.FrSuffix(), "", srcRanges, nodePorts, nodes)
 	if err != nil {
 		t.Errorf("failed to update firewall rule, err: %v", err)
 	}
-	verifyFirewallRule(fwp, ruleName, nodePorts, nodes, []string{all}, t)
+	verifyFirewallRule(fwp, ruleName, nodePorts, nodes, []string{allCIDR}, t)
 
 	// Run Sync and expect l7 src ranges to be returned
 	err = fp.Sync(nodePorts, nodes)
@@ -47,7 +64,7 @@ func TestSyncFirewallPool(t *testing.T) {
 	}
 	verifyFirewallRule(fwp, ruleName, nodePorts, nodes, l7SrcRanges, t)
 
-	// Add node and expect firwall to remain the same
+	// Add node and expect firewall to remain the same
 	// NOTE: See computeHostTag(..) in gce cloudprovider
 	nodes = []string{"node-a", "node-b", "node-c", "node-d"}
 	err = fp.Sync(nodePorts, nodes)
@@ -81,12 +98,12 @@ func verifyFirewallRule(fwp *fakeFirewallsProvider, ruleName string, expectedPor
 		t.Errorf("could not retrieve firewall via cloud api, err %v", err)
 	}
 
-	// Verify firwall rule has correct ports
+	// Verify firewall rule has correct ports
 	if !sets.NewString(f.Allowed[0].Ports...).Equal(sets.NewString(strPorts...)) {
 		t.Errorf("allowed ports doesn't equal expected ports, Actual: %v, Expected: %v", f.Allowed[0].Ports, strPorts)
 	}
 
-	// Verify firwall rule has correct CIDRs
+	// Verify firewall rule has correct CIDRs
 	if !sets.NewString(f.SourceRanges...).Equal(sets.NewString(expectedCIDRs...)) {
 		t.Errorf("source CIDRs doesn't equal expected CIDRs. Actual: %v, Expected: %v", f.SourceRanges, expectedCIDRs)
 	}
