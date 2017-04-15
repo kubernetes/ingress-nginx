nicksardo/ingress

Support backside re-encryption

controllers/gce/controller/util_test.go
@@ -100,11 +100,11 @@ func TestProbeGetter(t *testing.T) {
 	}
 	addPods(lbc, nodePortToHealthCheck, api_v1.NamespaceDefault)
 	for p, exp := range nodePortToHealthCheck {
-		got, err := lbc.tr.HealthCheck(p)
-		if err != nil {
-			t.Errorf("Failed to get health check for node port %v: %v", p, err)
-		} else if got.RequestPath != exp {
-			t.Errorf("Wrong health check for node port %v, got %v expected %v", p, got.RequestPath, exp)
+		got, err := lbc.tr.GetProbe(p)
+		if err != nil || got == nil {
+			t.Errorf("Failed to get probe for node port %v: %v", p, err)
+		} else if getPathInProbe(got) != exp {
+			t.Errorf("Wrong path for node port %v, got %v expected %v", p, getPathInProbe(got), exp)
 		}
 	}
 }
@@ -122,11 +122,11 @@ func TestProbeGetterNamedPort(t *testing.T) {
 		pod.Spec.Containers[0].ReadinessProbe.Handler.HTTPGet.Port = intstr.IntOrString{Type: intstr.String, StrVal: "test"}
 	}
 	for p, exp := range nodePortToHealthCheck {
-		got, err := lbc.tr.HealthCheck(p)
-		if err != nil {
-			t.Errorf("Failed to get health check for node port %v: %v", p, err)
-		} else if got.RequestPath != exp {
-			t.Errorf("Wrong health check for node port %v, got %v expected %v", p, got.RequestPath, exp)
+		got, err := lbc.tr.GetProbe(p)
+		if err != nil || got == nil {
+			t.Errorf("Failed to get probe for node port %v: %v", p, err)
+		} else if getPathInProbe(got) != exp {
+			t.Errorf("Wrong path for node port %v, got %v expected %v", p, getPathInProbe(got), exp)
 		}
 	}
 
@@ -173,11 +173,11 @@ func TestProbeGetterCrossNamespace(t *testing.T) {
 	addPods(lbc, nodePortToHealthCheck, api_v1.NamespaceDefault)
 
 	for p, exp := range nodePortToHealthCheck {
-		got, err := lbc.tr.HealthCheck(p)
-		if err != nil {
-			t.Errorf("Failed to get health check for node port %v: %v", p, err)
-		} else if got.RequestPath != exp {
-			t.Errorf("Wrong health check for node port %v, got %v expected %v", p, got.RequestPath, exp)
+		got, err := lbc.tr.GetProbe(p)
+		if err != nil || got == nil {
+			t.Errorf("Failed to get probe for node port %v: %v", p, err)
+		} else if getPathInProbe(got) != exp {
+			t.Errorf("Wrong path for node port %v, got %v expected %v", p, getPathInProbe(got), exp)
 		}
 	}
 }
@@ -256,4 +256,8 @@ func addNodes(lbc *LoadBalancerController, zoneToNode map[string][]string) {
 		}
 	}
 	lbc.CloudClusterManager.instancePool.Init(lbc.tr)
+}
+
+func getPathInProbe(p *api_v1.Probe) string {
+	return p.Handler.HTTPGet.Path
 }

controllers/gce/healthchecks/healthchecks_test.go
@@ -17,47 +17,149 @@ limitations under the License.
 package healthchecks
 
 import (
+	"net/http"
 	"testing"
 
 	"k8s.io/ingress/controllers/gce/utils"
 )
 
-func TestFakeHealthCheckActions(t *testing.T) {
+func TestHealthCheckAdd(t *testing.T) {
 	namer := &utils.Namer{}
-	healthChecks := NewHealthChecker(NewFakeHealthChecks(), "/", namer)
-	healthChecks.Init(&FakeHealthCheckGetter{DefaultHealthCheck: nil})
+	hcp := NewFakeHealthCheckProvider()
+	healthChecks := NewHealthChecker(hcp, "/", namer)
 
-	err := healthChecks.Add(80)
+	hc := healthChecks.New(80, false)
+	err := healthChecks.Sync(hc)
 	if err != nil {
-		t.Fatalf("unexpected error")
+		t.Fatalf("unexpected error: %v", err)
 	}
-
-	_, err1 := healthChecks.Get(8080)
-	if err1 == nil {
-		t.Errorf("expected error")
-	}
-
-	hc, err2 := healthChecks.Get(80)
-	if err2 != nil {
-		t.Errorf("unexpected error")
-	} else {
-		if hc == nil {
-			t.Errorf("expected a *compute.HttpHealthCheck")
-		}
-	}
-
-	err = healthChecks.Delete(8080)
-	if err == nil {
-		t.Errorf("expected error")
-	}
-
-	err = healthChecks.Delete(80)
+	// Verify the health check exists
+	_, err = hcp.GetHttpHealthCheck(namer.BeName(80))
 	if err != nil {
-		t.Errorf("unexpected error")
+		t.Fatalf("expected the health check to exist, err: %v", err)
 	}
 
-	_, err3 := healthChecks.Get(80)
-	if err3 == nil {
-		t.Errorf("expected error")
+	hc = healthChecks.New(443, true)
+	err = healthChecks.Sync(hc)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	// Verify the health check exists
+	_, err = hcp.GetHttpsHealthCheck(namer.BeName(443))
+	if err != nil {
+		t.Fatalf("expected the health check to exist, err: %v", err)
+	}
+}
+
+func TestHealthCheckAddExisting(t *testing.T) {
+	namer := &utils.Namer{}
+	hcp := NewFakeHealthCheckProvider()
+	healthChecks := NewHealthChecker(hcp, "/", namer)
+
+	// HTTP
+	// Manually insert a health check
+	httpHC := DefaultHealthCheckTemplate(3000, false)
+	httpHC.Name = namer.BeName(3000)
+	httpHC.RequestPath = "/my-probes-health"
+	hcp.CreateHttpHealthCheck(httpHC.ToHttpHealthCheck())
+
+	// Should not fail adding the same type of health check
+	hc := healthChecks.New(3000, false)
+	err := healthChecks.Sync(hc)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	// Verify the health check exists
+	_, err = hcp.GetHttpHealthCheck(httpHC.Name)
+	if err != nil {
+		t.Fatalf("expected the health check to continue existing, err: %v", err)
+	}
+
+	// HTTPS
+	// Manually insert a health check
+	httpsHC := DefaultHealthCheckTemplate(4000, true)
+	httpsHC.Name = namer.BeName(4000)
+	httpsHC.RequestPath = "/my-probes-health"
+	hcp.CreateHttpsHealthCheck(httpsHC.ToHttpsHealthCheck())
+
+	hc = healthChecks.New(4000, true)
+	err = healthChecks.Sync(hc)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	// Verify the health check exists
+	_, err = hcp.GetHttpsHealthCheck(httpsHC.Name)
+	if err != nil {
+		t.Fatalf("expected the health check to continue existing, err: %v", err)
+	}
+}
+
+func TestHealthCheckDelete(t *testing.T) {
+	namer := &utils.Namer{}
+	hcp := NewFakeHealthCheckProvider()
+	healthChecks := NewHealthChecker(hcp, "/", namer)
+
+	// Create HTTP HC for 1234
+	hc := DefaultHealthCheckTemplate(1234, false)
+	hc.Name = namer.BeName(1234)
+	hcp.CreateHttpHealthCheck(hc.ToHttpHealthCheck())
+
+	// Create HTTPS HC for 1234)
+	hcp.CreateHttpsHealthCheck(hc.ToHttpsHealthCheck())
+
+	// Delete only HTTP 1234
+	err := healthChecks.Delete(1234, false)
+	if err != nil {
+		t.Errorf("unexpected error when deleting HTTP health check, err: %v", err)
+	}
+
+	// Validate port is deleted
+	_, err = hcp.GetHttpHealthCheck(hc.Name)
+	if !utils.IsHTTPErrorCode(err, http.StatusNotFound) {
+		t.Errorf("expected not-found error, actual: %v", err)
+	}
+
+	// Validate the wrong port wasn't deleted
+	_, err = hcp.GetHttpsHealthCheck(hc.Name)
+	if err != nil {
+		t.Errorf("unexpected error retrieving known healthcheck: %v", err)
+	}
+
+	// Delete only HTTPS 1234
+	err = healthChecks.Delete(1234, true)
+	if err != nil {
+		t.Errorf("unexpected error when deleting HTTPS health check, err: %v", err)
+	}
+}
+
+func TestHealthCheckGet(t *testing.T) {
+	namer := &utils.Namer{}
+	hcp := NewFakeHealthCheckProvider()
+	healthChecks := NewHealthChecker(hcp, "/", namer)
+
+	// HTTP
+	// Manually insert a health check
+	httpHC := DefaultHealthCheckTemplate(3000, false)
+	httpHC.Name = namer.BeName(3000)
+	httpHC.RequestPath = "/my-probes-health"
+	hcp.CreateHttpHealthCheck(httpHC.ToHttpHealthCheck())
+
+	// Verify the health check exists
+	_, err := healthChecks.Get(3000, false)
+	if err != nil {
+		t.Fatalf("expected the health check to exist, err: %v", err)
+	}
+
+	// HTTPS
+	// Manually insert a health check
+	httpsHC := DefaultHealthCheckTemplate(4000, true)
+	httpsHC.Name = namer.BeName(4000)
+	httpsHC.RequestPath = "/my-probes-health"
+	hcp.CreateHttpsHealthCheck(httpsHC.ToHttpsHealthCheck())
+
+	// Verify the health check exists
+	_, err = healthChecks.Get(4000, true)
+	if err != nil {
+		t.Fatalf("expected the health check to exist, err: %v", err)
 	}
 }

controllers/gce/healthchecks/interfaces.go
@@ -16,29 +16,25 @@ limitations under the License.
 
 package healthchecks
 
-import (
-	compute "google.golang.org/api/compute/v1"
-)
+import compute "google.golang.org/api/compute/v1"
 
-// healthCheckGetter retrieves health checks.
-type healthCheckGetter interface {
-	// HealthCheck returns the HTTP readiness check for a node port.
-	HealthCheck(nodePort int64) (*compute.HttpHealthCheck, error)
-}
-
-// SingleHealthCheck is an interface to manage a single GCE health check.
-type SingleHealthCheck interface {
+// HealthCheckProvider is an interface to manage a single GCE health check.
+type HealthCheckProvider interface {
 	CreateHttpHealthCheck(hc *compute.HttpHealthCheck) error
 	UpdateHttpHealthCheck(hc *compute.HttpHealthCheck) error
 	DeleteHttpHealthCheck(name string) error
 	GetHttpHealthCheck(name string) (*compute.HttpHealthCheck, error)
+
+	CreateHttpsHealthCheck(hc *compute.HttpsHealthCheck) error
+	UpdateHttpsHealthCheck(hc *compute.HttpsHealthCheck) error
+	DeleteHttpsHealthCheck(name string) error
+	GetHttpsHealthCheck(name string) (*compute.HttpsHealthCheck, error)
 }
 
 // HealthChecker is an interface to manage cloud HTTPHealthChecks.
 type HealthChecker interface {
-	Init(h healthCheckGetter)
-
-	Add(port int64) error
-	Delete(port int64) error
-	Get(port int64) (*compute.HttpHealthCheck, error)
+	New(port int64, encrypted bool) *HealthCheck
+	Sync(hc *HealthCheck) error
+	Delete(port int64, encrypted bool) error
+	Get(port int64, encrypted bool) (*HealthCheck, error)
 }

kubernetes/ingress-nginx

controllers/gce/controller/util_test.go
@@ -100,11 +100,11 @@ func TestProbeGetter(t *testing.T) {
 	}
 	addPods(lbc, nodePortToHealthCheck, api_v1.NamespaceDefault)
 	for p, exp := range nodePortToHealthCheck {
-		got, err := lbc.tr.HealthCheck(p)
-		if err != nil {
-			t.Errorf("Failed to get health check for node port %v: %v", p, err)
-		} else if got.RequestPath != exp {
-			t.Errorf("Wrong health check for node port %v, got %v expected %v", p, got.RequestPath, exp)
+		got, err := lbc.tr.GetProbe(p)
+		if err != nil || got == nil {
+			t.Errorf("Failed to get probe for node port %v: %v", p, err)
+		} else if getProbePath(got) != exp {
+			t.Errorf("Wrong path for node port %v, got %v expected %v", p, getProbePath(got), exp)
 		}
 	}
 }
@@ -122,11 +122,11 @@ func TestProbeGetterNamedPort(t *testing.T) {
 		pod.Spec.Containers[0].ReadinessProbe.Handler.HTTPGet.Port = intstr.IntOrString{Type: intstr.String, StrVal: "test"}
 	}
 	for p, exp := range nodePortToHealthCheck {
-		got, err := lbc.tr.HealthCheck(p)
-		if err != nil {
-			t.Errorf("Failed to get health check for node port %v: %v", p, err)
-		} else if got.RequestPath != exp {
-			t.Errorf("Wrong health check for node port %v, got %v expected %v", p, got.RequestPath, exp)
+		got, err := lbc.tr.GetProbe(p)
+		if err != nil || got == nil {
+			t.Errorf("Failed to get probe for node port %v: %v", p, err)
+		} else if getProbePath(got) != exp {
+			t.Errorf("Wrong path for node port %v, got %v expected %v", p, getProbePath(got), exp)
 		}
 	}
 
@@ -173,11 +173,11 @@ func TestProbeGetterCrossNamespace(t *testing.T) {
 	addPods(lbc, nodePortToHealthCheck, api_v1.NamespaceDefault)
 
 	for p, exp := range nodePortToHealthCheck {
-		got, err := lbc.tr.HealthCheck(p)
-		if err != nil {
-			t.Errorf("Failed to get health check for node port %v: %v", p, err)
-		} else if got.RequestPath != exp {
-			t.Errorf("Wrong health check for node port %v, got %v expected %v", p, got.RequestPath, exp)
+		got, err := lbc.tr.GetProbe(p)
+		if err != nil || got == nil {
+			t.Errorf("Failed to get probe for node port %v: %v", p, err)
+		} else if getProbePath(got) != exp {
+			t.Errorf("Wrong path for node port %v, got %v expected %v", p, getProbePath(got), exp)
 		}
 	}
 }
@@ -256,4 +256,8 @@ func addNodes(lbc *LoadBalancerController, zoneToNode map[string][]string) {
 		}
 	}
 	lbc.CloudClusterManager.instancePool.Init(lbc.tr)
+}
+
+func getProbePath(p *api_v1.Probe) string {
+	return p.Handler.HTTPGet.Path
 }

controllers/gce/healthchecks/healthchecks_test.go
@@ -17,47 +17,170 @@ limitations under the License.
 package healthchecks
 
 import (
+	"net/http"
 	"testing"
+
+	compute "google.golang.org/api/compute/v1"
 
 	"k8s.io/ingress/controllers/gce/utils"
 )
 
-func TestFakeHealthCheckActions(t *testing.T) {
+func TestHealthCheckAdd(t *testing.T) {
+	namer := utils.NewNamer("ABC", "XYZ")
+	hcp := NewFakeHealthCheckProvider()
+	healthChecks := NewHealthChecker(hcp, "/", namer)
+
+	hc := healthChecks.New(80, utils.HTTP)
+	_, err := healthChecks.Sync(hc)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	// Verify the health check exists
+	_, err = hcp.GetHealthCheck(namer.BeName(80))
+	if err != nil {
+		t.Fatalf("expected the health check to exist, err: %v", err)
+	}
+
+	hc = healthChecks.New(443, utils.HTTPS)
+	_, err = healthChecks.Sync(hc)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	// Verify the health check exists
+	_, err = hcp.GetHealthCheck(namer.BeName(443))
+	if err != nil {
+		t.Fatalf("expected the health check to exist, err: %v", err)
+	}
+}
+
+func TestHealthCheckAddExisting(t *testing.T) {
 	namer := &utils.Namer{}
-	healthChecks := NewHealthChecker(NewFakeHealthChecks(), "/", namer)
-	healthChecks.Init(&FakeHealthCheckGetter{DefaultHealthCheck: nil})
+	hcp := NewFakeHealthCheckProvider()
+	healthChecks := NewHealthChecker(hcp, "/", namer)
 
-	err := healthChecks.Add(80)
+	// HTTP
+	// Manually insert a health check
+	httpHC := DefaultHealthCheck(3000, utils.HTTP)
+	httpHC.Name = namer.BeName(3000)
+	httpHC.RequestPath = "/my-probes-health"
+	hcp.CreateHealthCheck(httpHC.Out())
+
+	// Should not fail adding the same type of health check
+	hc := healthChecks.New(3000, utils.HTTP)
+	_, err := healthChecks.Sync(hc)
 	if err != nil {
-		t.Fatalf("unexpected error")
+		t.Fatalf("unexpected error: %v", err)
+	}
+	// Verify the health check exists
+	_, err = hcp.GetHealthCheck(httpHC.Name)
+	if err != nil {
+		t.Fatalf("expected the health check to continue existing, err: %v", err)
 	}
 
-	_, err1 := healthChecks.Get(8080)
-	if err1 == nil {
-		t.Errorf("expected error")
+	// HTTPS
+	// Manually insert a health check
+	httpsHC := DefaultHealthCheck(4000, utils.HTTPS)
+	httpsHC.Name = namer.BeName(4000)
+	httpsHC.RequestPath = "/my-probes-health"
+	hcp.CreateHealthCheck(httpsHC.Out())
+
+	hc = healthChecks.New(4000, utils.HTTPS)
+	_, err = healthChecks.Sync(hc)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	// Verify the health check exists
+	_, err = hcp.GetHealthCheck(httpsHC.Name)
+	if err != nil {
+		t.Fatalf("expected the health check to continue existing, err: %v", err)
+	}
+}
+
+func TestHealthCheckDelete(t *testing.T) {
+	namer := &utils.Namer{}
+	hcp := NewFakeHealthCheckProvider()
+	healthChecks := NewHealthChecker(hcp, "/", namer)
+
+	// Create HTTP HC for 1234
+	hc := DefaultHealthCheck(1234, utils.HTTP)
+	hc.Name = namer.BeName(1234)
+	hcp.CreateHealthCheck(hc.Out())
+
+	// Create HTTPS HC for 1234)
+	hc.Type = string(utils.HTTPS)
+	hcp.CreateHealthCheck(hc.Out())
+
+	// Delete only HTTP 1234
+	err := healthChecks.Delete(1234)
+	if err != nil {
+		t.Errorf("unexpected error when deleting health check, err: %v", err)
 	}
 
-	hc, err2 := healthChecks.Get(80)
-	if err2 != nil {
-		t.Errorf("unexpected error")
-	} else {
-		if hc == nil {
-			t.Errorf("expected a *compute.HttpHealthCheck")
-		}
+	// Validate port is deleted
+	_, err = hcp.GetHealthCheck(hc.Name)
+	if !utils.IsHTTPErrorCode(err, http.StatusNotFound) {
+		t.Errorf("expected not-found error, actual: %v", err)
 	}
 
-	err = healthChecks.Delete(8080)
+	// Delete only HTTP 1234
+	err = healthChecks.Delete(1234)
 	if err == nil {
-		t.Errorf("expected error")
+		t.Errorf("expected not-found error when deleting health check, err: %v", err)
 	}
+}
 
-	err = healthChecks.Delete(80)
+func TestHealthCheckUpdate(t *testing.T) {
+	namer := &utils.Namer{}
+	hcp := NewFakeHealthCheckProvider()
+	healthChecks := NewHealthChecker(hcp, "/", namer)
+
+	// HTTP
+	// Manually insert a health check
+	hc := DefaultHealthCheck(3000, utils.HTTP)
+	hc.Name = namer.BeName(3000)
+	hc.RequestPath = "/my-probes-health"
+	hcp.CreateHealthCheck(hc.Out())
+
+	// Verify the health check exists
+	_, err := healthChecks.Get(3000)
 	if err != nil {
-		t.Errorf("unexpected error")
+		t.Fatalf("expected the health check to exist, err: %v", err)
 	}
 
-	_, err3 := healthChecks.Get(80)
-	if err3 == nil {
-		t.Errorf("expected error")
+	// Change to HTTPS
+	hc.Type = string(utils.HTTPS)
+	_, err = healthChecks.Sync(hc)
+	if err != nil {
+		t.Fatalf("unexpected err while syncing healthcheck, err %v", err)
 	}
+
+	// Verify the health check exists
+	_, err = healthChecks.Get(3000)
+	if err != nil {
+		t.Fatalf("expected the health check to exist, err: %v", err)
+	}
+
+	// Verify the check is now HTTPS
+	if hc.Protocol() != utils.HTTPS {
+		t.Fatalf("expected check to be of type HTTPS")
+	}
+}
+
+func TestHealthCheckDeleteLegacy(t *testing.T) {
+	namer := &utils.Namer{}
+	hcp := NewFakeHealthCheckProvider()
+	healthChecks := NewHealthChecker(hcp, "/", namer)
+
+	err := hcp.CreateHttpHealthCheck(&compute.HttpHealthCheck{
+		Name: namer.BeName(80),
+	})
+	if err != nil {
+		t.Fatalf("expected health check to be created, err: %v", err)
+	}
+
+	err = healthChecks.DeleteLegacy(80)
+	if err != nil {
+		t.Fatalf("expected health check to be deleted, err: %v", err)
+	}
+
 }

controllers/gce/healthchecks/interfaces.go
@@ -18,27 +18,28 @@ package healthchecks
 
 import (
 	compute "google.golang.org/api/compute/v1"
+
+	"k8s.io/ingress/controllers/gce/utils"
 )
 
-// healthCheckGetter retrieves health checks.
-type healthCheckGetter interface {
-	// HealthCheck returns the HTTP readiness check for a node port.
-	HealthCheck(nodePort int64) (*compute.HttpHealthCheck, error)
-}
-
-// SingleHealthCheck is an interface to manage a single GCE health check.
-type SingleHealthCheck interface {
+// HealthCheckProvider is an interface to manage a single GCE health check.
+type HealthCheckProvider interface {
 	CreateHttpHealthCheck(hc *compute.HttpHealthCheck) error
 	UpdateHttpHealthCheck(hc *compute.HttpHealthCheck) error
 	DeleteHttpHealthCheck(name string) error
 	GetHttpHealthCheck(name string) (*compute.HttpHealthCheck, error)
+
+	CreateHealthCheck(hc *compute.HealthCheck) error
+	UpdateHealthCheck(hc *compute.HealthCheck) error
+	DeleteHealthCheck(name string) error
+	GetHealthCheck(name string) (*compute.HealthCheck, error)
 }
 
 // HealthChecker is an interface to manage cloud HTTPHealthChecks.
 type HealthChecker interface {
-	Init(h healthCheckGetter)
-
-	Add(port int64) error
+	New(port int64, protocol utils.AppProtocol) *HealthCheck
+	Sync(hc *HealthCheck) (string, error)
 	Delete(port int64) error
-	Get(port int64) (*compute.HttpHealthCheck, error)
+	Get(port int64) (*HealthCheck, error)
+	DeleteLegacy(port int64) error
 }