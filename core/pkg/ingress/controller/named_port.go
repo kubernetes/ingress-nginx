5e913cc8bd9ea4ed0f6a119fdec9a8f8650dd03c
Manuel de Brito Fontes <aledbf@gmail.com>
2017-05-22 22:55:39 -0400

    Remove service annotation for namedPorts

--- core/pkg/ingress/controller/named_port.go
+++ core/pkg/ingress/controller/named_port.go
@@ -17,93 +17,11 @@ limitations under the License.
 package controller
 
 import (
-	"encoding/json"
 	"fmt"
-	"reflect"
-	"strconv"
 
-	"github.com/golang/glog"
-
-	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	api_v1 "k8s.io/client-go/pkg/api/v1"
-
-	"k8s.io/ingress/core/pkg/ingress/annotations/service"
 )
-
-// checkSvcForUpdate verifies if one of the running pods for a service contains
-// named port. If the annotation in the service does not exist or is not equals
-// to the port mapping obtained from the pod the service must be updated to reflect
-// the current state
-func (ic *GenericController) checkSvcForUpdate(svc *api_v1.Service) error {
-	// get the pods associated with the service
-	// TODO: switch this to a watch
-	pods, err := ic.cfg.Client.Core().Pods(svc.Namespace).List(meta_v1.ListOptions{
-		LabelSelector: labels.Set(svc.Spec.Selector).AsSelector().String(),
-	})
-
-	if err != nil {
-		return fmt.Errorf("error searching service pods %v/%v: %v", svc.Namespace, svc.Name, err)
-	}
-
-	if len(pods.Items) == 0 {
-		return nil
-	}
-
-	namedPorts := map[string]string{}
-
-	// we need to check only one pod searching for named ports
-	pod := &pods.Items[0]
-	glog.V(4).Infof("checking pod %v/%v for named port information", pod.Namespace, pod.Name)
-	for i := range svc.Spec.Ports {
-		servicePort := &svc.Spec.Ports[i]
-
-		_, err := strconv.Atoi(servicePort.TargetPort.StrVal)
-		if err != nil {
-			portNum, err := findPort(pod, servicePort)
-			if err != nil {
-				glog.V(4).Infof("failed to find port for service %s/%s: %v", portNum, svc.Namespace, svc.Name, err)
-				continue
-			}
-
-			if servicePort.TargetPort.StrVal == "" {
-				continue
-			}
-
-			namedPorts[servicePort.TargetPort.StrVal] = fmt.Sprintf("%v", portNum)
-		}
-	}
-
-	if svc.ObjectMeta.Annotations == nil {
-		svc.ObjectMeta.Annotations = map[string]string{}
-	}
-
-	curNamedPort := svc.ObjectMeta.Annotations[service.NamedPortAnnotation]
-	if len(namedPorts) > 0 && !reflect.DeepEqual(curNamedPort, namedPorts) {
-		data, _ := json.Marshal(namedPorts)
-
-		newSvc, err := ic.cfg.Client.Core().Services(svc.Namespace).Get(svc.Name, meta_v1.GetOptions{})
-		if err != nil {
-			return fmt.Errorf("error getting service %v/%v: %v", svc.Namespace, svc.Name, err)
-		}
-
-		if newSvc.ObjectMeta.Annotations == nil {
-			newSvc.ObjectMeta.Annotations = map[string]string{}
-		}
-
-		newSvc.ObjectMeta.Annotations[service.NamedPortAnnotation] = string(data)
-		glog.Infof("updating service %v with new named port mappings", svc.Name)
-		_, err = ic.cfg.Client.Core().Services(svc.Namespace).Update(newSvc)
-		if err != nil {
-			return fmt.Errorf("error syncing service %v/%v: %v", svc.Namespace, svc.Name, err)
-		}
-
-		return nil
-	}
-
-	return nil
-}
 
 // FindPort locates the container port for the given pod and portName.  If the
 // targetPort is a number, use that.  If the targetPort is a string, look that
