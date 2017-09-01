7cbb812fbee418f7ad901520fe6ffd02f86e3f7d
Arno Uhlig <arno.uhlig@sap.com>
2017-09-01 17:43:11 +0200

    configurable proxy-request-buffering per location

--- core/pkg/ingress/annotations/proxy/main_test.go
+++ core/pkg/ingress/annotations/proxy/main_test.go
@@ -75,6 +75,7 @@ func (m mockBackend) GetDefaultBackend() defaults.Backend {
 		ProxyBodySize:       "3k",
 		ProxyNextUpstream:   "error",
 		ProxyPassParams:     "nocanon keepalive=On",
+		ProxyRequestBuffering: "on",
 	}
 }
 
@@ -89,6 +90,7 @@ func TestProxy(t *testing.T) {
 	data[bodySize] = "2k"
 	data[nextUpstream] = "off"
 	data[passParams] = "smax=5 max=10"
+	data[requestBuffering] = "off"
 	ing.SetAnnotations(data)
 
 	i, err := NewParser(mockBackend{}).Parse(ing)
@@ -119,6 +121,9 @@ func TestProxy(t *testing.T) {
 	}
 	if p.PassParams != "smax=5 max=10" {
 		t.Errorf("expected \"smax=5 max=10\" as pass-params but returned \"%v\"", p.PassParams)
+	}
+	if p.RequestBuffering != "off" {
+		t.Errorf("expected off as request-buffering but returned %v", p.RequestBuffering)
 	}
 }
 
@@ -156,5 +161,8 @@ func TestProxyWithNoAnnotation(t *testing.T) {
 	}
 	if p.PassParams != "nocanon keepalive=On" {
 		t.Errorf("expected \"nocanon keepalive=On\" as pass-params but returned \"%v\"", p.PassParams)
+	}
+	if p.RequestBuffering != "on" {
+		t.Errorf("expected on as request-buffering but returned %v", p.RequestBuffering)
 	}
 }
