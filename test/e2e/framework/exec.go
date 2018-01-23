8ffd1c98d261ddb157af4a6be2717a0f58963f33
Manuel de Brito Fontes <aledbf@gmail.com>
2018-01-23 17:09:41 -0300

    Add container flag where is required to run the command

--- test/e2e/framework/exec.go
+++ test/e2e/framework/exec.go
@@ -31,11 +31,12 @@ func (f *Framework) ExecCommand(pod *v1.Pod, command string) (string, error) {
 		execErr bytes.Buffer
 	)
 
+	args := fmt.Sprintf("kubectl exec --namespace %v %v -- %v", pod.Namespace, pod.Name, command)
 	if len(pod.Spec.Containers) != 1 {
-		return "", fmt.Errorf("could not determine which container to use")
+		args = fmt.Sprintf("kubectl exec --namespace %v %v --container nginx-ingress-controller -- %v", pod.Namespace, pod.Name, command)
 	}
 
-	args := fmt.Sprintf("kubectl exec -n %v %v -- %v", pod.Namespace, pod.Name, command)
+	log("DEBUG", "Executing command \"%v\"", args)
 	cmd := exec.Command("/bin/bash", "-c", args)
 	cmd.Stdout = &execOut
 	cmd.Stderr = &execErr
