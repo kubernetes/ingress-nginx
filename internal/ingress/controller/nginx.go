385368990cd678d08fe0b30fd8f087cfaad91a19
Sylvain Rabot <sylvain@abstraction.fr>
2018-03-28 14:27:34 +0200

    Managing a whitelist for _/nginx_status (#2187)
    Signed-off-by: Sylvain Rabot <s.rabot@lectra.com>

--- internal/ingress/controller/nginx.go
+++ internal/ingress/controller/nginx.go
@@ -621,6 +621,8 @@ func (n *NGINXController) OnUpdate(ingressCfg ingress.Configuration) error {
 		CustomErrors:                len(cfg.CustomHTTPErrors) > 0,
 		Cfg:                         cfg,
 		IsIPV6Enabled:               n.isIPV6Enabled && !cfg.DisableIpv6,
+		NginxStatusIpv4Whitelist:    cfg.NginxStatusIpv4Whitelist,
+		NginxStatusIpv6Whitelist:    cfg.NginxStatusIpv6Whitelist,
 		RedirectServers:             redirectServers,
 		IsSSLPassthroughEnabled:     n.cfg.EnableSSLPassthrough,
 		ListenPorts:                 n.cfg.ListenPorts,
