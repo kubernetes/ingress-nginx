51336a3677107b1a41ff8139eabd14b923c39af3
Andrew Louis <andrew.louis@shopify.com>
2018-04-05 15:03:53 -0400

    Actually read from the file

--- rootfs/etc/nginx/lua/configuration.lua
+++ rootfs/etc/nginx/lua/configuration.lua
@@ -7,6 +7,27 @@ function _M.get_backends_data()
   return configuration_data:get("backends")
 end
 
+function _M.new_backends()
+  ngx.req.read_body()
+  local backends = ngx.req.get_body_data()
+  if not backends then
+    -- response might be have been written to tmp file if size(body) > client_body_buffer_size
+    local file_name = ngx.req.get_body_file()
+    local file = assert(io.open(file_name, "rb"))
+    backends = file:read("*all")
+    file:close()
+  end
+
+  if not backends then
+    -- no current backends data found
+    ngx.log(ngx.ERR, "configuration/backends: empty response body")
+    ngx.status = ngx.HTTP_BAD_REQUEST
+    return
+  end
+
+  return backends
+end
+
 function _M.call()
   if ngx.var.request_method ~= "POST" and ngx.var.request_method ~= "GET" then
     ngx.status = ngx.HTTP_BAD_REQUEST
@@ -26,20 +47,7 @@ function _M.call()
     return
   end
 
-  ngx.req.read_body()
-  local backends = ngx.req.get_body_data()
-  if not backends then
-    -- response might be have been written to tmp file if size(body) > client_body_buffer_size
-    backends = ngx.req.get_body_file()
-  end
-  if not backends then
-    -- no current backends data found
-    ngx.log(ngx.ERR, "configuration/backends: empty response body")
-    ngx.status = ngx.HTTP_BAD_REQUEST
-    return
-  end
-
-  local success, err = configuration_data:set("backends", ngx.req.get_body_data())
+  local success, err = configuration_data:set("backends", _M.new_backends())
   if not success then
     ngx.log(ngx.ERR, "configuration/backends: error updating configuration: " .. tostring(err))
     ngx.status = ngx.HTTP_BAD_REQUEST
