3f79f27b65cd10bd228bbdafb7318c4904b27151
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2018-04-10 19:52:58 -0300

    Update nginx image (#2328)
    * Update nginx image
    
    * Update minikube start script

--- test/e2e/up.sh
+++ test/e2e/up.sh
@@ -21,7 +21,7 @@ curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$KUBE
     chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 
 echo "downloading minikube..."
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && \
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.25.2/minikube-linux-amd64 && \
     chmod +x minikube && \
     sudo mv minikube /usr/local/bin/
 
