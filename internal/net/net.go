123ffc0c387671f8109d4bee8519fce795249916
Manuel de Brito Fontes <aledbf@gmail.com>
2018-08-06 12:13:18 -0400

    Fix IPV6 detection

--- internal/net/net.go
+++ internal/net/net.go
@@ -24,7 +24,7 @@ import (
 
 // IsIPV6 checks if the input contains a valid IPV6 address
 func IsIPV6(ip _net.IP) bool {
-	return ip.To4() == nil
+	return ip != nil && ip.To4() == nil
 }
 
 // IsPortAvailable checks if a TCP port is available or not
@@ -37,8 +37,25 @@ func IsPortAvailable(p int) bool {
 	return false
 }
 
-// IsIPv6Enabled checks if IPV6 is enabled or not
+// IsIPv6Enabled checks if IPV6 is enabled or not and we have
+// at least one configured in the pod
 func IsIPv6Enabled() bool {
 	cmd := exec.Command("test", "-f", "/proc/net/if_inet6")
-	return cmd.Run() == nil
+	if cmd.Run() != nil {
+		return false
+	}
+
+	addrs, err := _net.InterfaceAddrs()
+	if err != nil {
+		return false
+	}
+
+	for _, addr := range addrs {
+		ip, _, _ := _net.ParseCIDR(addr.String())
+		if IsIPV6(ip) {
+			return true
+		}
+	}
+
+	return false
 }
