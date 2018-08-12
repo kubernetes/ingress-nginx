7450b5cfa60423de8d2a8800a5b1d661e698ace4
Manuel de Brito Fontes <aledbf@gmail.com>
2018-08-12 18:53:45 -0300

    Migrate configuration configmap to CRDs

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -34,3 +34,16 @@ ${CODEGEN_PKG}/generate-groups.sh "deepcopy" \
   k8s.io/ingress-nginx/internal k8s.io/ingress-nginx/internal \
   .:ingress \
   --output-base "$(dirname ${BASH_SOURCE})/../../.."
+
+${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
+  k8s.io/ingress-nginx/internal/apis k8s.io/ingress-nginx/internal/apis \
+  configuration:v1alpha1 \
+  --output-base "$(dirname ${BASH_SOURCE})/../../.." \
+  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate/boilerplate.go.txt
+
+${GOPATH}/bin/conversion-gen || go build -o "${GOPATH}/bin/conversion-gen" "${CODEGEN_PKG}/cmd/conversion-gen"
+
+${GOPATH}/bin/conversion-gen --skip-unsafe=true \
+  --input-dirs k8s.io/ingress-nginx/internal/apis/configuration/v1alpha1 \
+  --output-file-base=zz_generated.conversion \
+  --go-header-file "hack/boilerplate/boilerplate.go.txt"
---
+++ internal/apis/configuration/v1alpha1/types.go
@@ -0,0 +1,67 @@
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Configuration represents the NGINX Ingress controller configuration.
+type Configuration struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard object's metadata.
+	// More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#metadata
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+
+	// Specification of the desired configuration
+	// +optional
+	Spec ConfigurationSpec `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"`
+
+	// Current status of the configuration.
+	// +optional
+	Status ConfigurationStatus `json:"status,omitempty" protobuf:"bytes,3,opt,name=status"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ConfigurationList is a collection of configurations.
+type ConfigurationList struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// Standard list metadata.
+	// More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#metadata
+	// +optional
+	metav1.ListMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
+
+	// items is the list of Configuration.
+	Items []Configuration `json:"items" protobuf:"bytes,2,rep,name=items"`
+}
+
+// ConfigurationSpec describes how the job execution will look like and when it will actually run.
+type ConfigurationSpec struct {
+}
+
+// ConfigurationStatus represents the current state of a configuration.
+type ConfigurationStatus struct {
+}
