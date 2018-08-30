17d56885ba0e5d115924f5e10a29b6fc47d78878
Manuel de Brito Fontes <aledbf@gmail.com>
2018-08-30 15:12:00 -0300

    Abort execution if there is any error running scripts

--- .travis/publish-docs.sh
+++ .travis/publish-docs.sh
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -o errexit
+set -o pipefail
+
 if ! [ -z $DEBUG ]; then
   set -x
 fi
