8978e75ed42fda0bf50eecff5b4fdd5ca6bb779d
Zenara Daley <zenara.daley@shopify.com>
2018-10-01 13:54:11 -0400

    Add 'use regex' annotation to toggle nginx regex location modifier

--- internal/ingress/annotations/rewrite/main.go
+++ internal/ingress/annotations/rewrite/main.go
@@ -38,6 +38,8 @@ type Config struct {
 	ForceSSLRedirect bool `json:"forceSSLRedirect"`
 	// AppRoot defines the Application Root that the Controller must redirect if it's in '/' context
 	AppRoot string `json:"appRoot"`
+	// UseRegex indicates whether or not the locations use regex paths
+	UseRegex bool `json:useRegex`
 }
 
 // Equal tests for equality between two Redirect types
@@ -64,6 +66,9 @@ func (r1 *Config) Equal(r2 *Config) bool {
 		return false
 	}
 	if r1.AppRoot != r2.AppRoot {
+		return false
+	}
+	if r1.UseRegex != r2.UseRegex {
 		return false
 	}
 
@@ -94,6 +99,7 @@ func (a rewrite) Parse(ing *extensions.Ingress) (interface{}, error) {
 	abu, _ := parser.GetBoolAnnotation("add-base-url", ing)
 	bus, _ := parser.GetStringAnnotation("base-url-scheme", ing)
 	ar, _ := parser.GetStringAnnotation("app-root", ing)
+	ur, _ := parser.GetBoolAnnotation("use-regex", ing)
 
 	return &Config{
 		Target:           rt,
@@ -102,5 +108,6 @@ func (a rewrite) Parse(ing *extensions.Ingress) (interface{}, error) {
 		SSLRedirect:      sslRe,
 		ForceSSLRedirect: fSslRe,
 		AppRoot:          ar,
+		UseRegex:         ur,
 	}, nil
 }
