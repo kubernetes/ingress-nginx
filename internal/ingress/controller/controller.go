zubchenok/ingress

Update lua configuration_data when number of controller pod change

internal/ingress/controller/controller.go
@@ -161,6 +161,7 @@ func (n *NGINXController) syncIngress(interface{}) error {
 		UDPEndpoints:          n.getStreamServices(n.cfg.UDPConfigMapName, apiv1.ProtocolUDP),
 		PassthroughBackends:   passUpstreams,
 		BackendConfigChecksum: n.store.GetBackendConfiguration().Checksum,
+		ControllerPods:        n.store.ListRunningControllerPods(),
 	}
 
 	if n.runningConfig.Equal(pcfg) {

kubernetes/ingress-nginx

internal/ingress/controller/controller.go
@@ -163,6 +163,7 @@ func (n *NGINXController) syncIngress(interface{}) error {
 		UDPEndpoints:          n.getStreamServices(n.cfg.UDPConfigMapName, apiv1.ProtocolUDP),
 		PassthroughBackends:   passUpstreams,
 		BackendConfigChecksum: n.store.GetBackendConfiguration().Checksum,
+		ControllerPodsCount:   n.store.GetRunningControllerPodsCount(),
 	}
 
 	if n.runningConfig.Equal(pcfg) {