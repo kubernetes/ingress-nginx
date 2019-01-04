811075a0c8a89c4f7d4847e8dd052be292ec8afc
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2019-01-03 23:09:56 -0300

    Update nginx to fix geoip database deprecation

--- images/nginx/rootfs/build.sh
+++ images/nginx/rootfs/build.sh
@@ -109,18 +109,16 @@ mkdir -p /etc/nginx
 # Get the GeoIP data
 GEOIP_FOLDER=/etc/nginx/geoip
 mkdir -p $GEOIP_FOLDER
-function geoip_get {
-  wget -O $GEOIP_FOLDER/$1 $2 || { echo "Could not download $1, exiting." ; exit 1; }
-  gunzip $GEOIP_FOLDER/$1
-}
+
 function geoip2_get {
   wget -O $GEOIP_FOLDER/$1.tar.gz $2 || { echo "Could not download $1, exiting." ; exit 1; }
-  mkdir $GEOIP_FOLDER/$1 && tar xf $GEOIP_FOLDER/$1.tar.gz -C $GEOIP_FOLDER/$1 --strip-components 1 && mv $GEOIP_FOLDER/$1/$1.mmdb $GEOIP_FOLDER/$1.mmdb && rm -rf $GEOIP_FOLDER/$1
+  mkdir $GEOIP_FOLDER/$1 \
+    && tar xf $GEOIP_FOLDER/$1.tar.gz -C $GEOIP_FOLDER/$1 --strip-components 1 \
+    && mv $GEOIP_FOLDER/$1/$1.mmdb $GEOIP_FOLDER/$1.mmdb \
+    && rm -rf $GEOIP_FOLDER/$1 \
+    && rm -rf $GEOIP_FOLDER/$1.tar.gz
 }
 
-geoip_get "GeoIPASNum.dat.gz"  "http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNum.dat.gz"
-geoip_get "GeoIP.dat.gz"       "https://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz"
-geoip_get "GeoLiteCity.dat.gz" "https://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz"
 geoip2_get "GeoLite2-City"     "http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz"
 geoip2_get "GeoLite2-ASN"      "http://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz"
 
@@ -566,6 +564,10 @@ rm -rf /etc/nginx/owasp-modsecurity-crs/.git
 rm -rf /etc/nginx/owasp-modsecurity-crs/util/regression-tests
 
 rm -rf $HOME/.hunter
+
+# move geoip directory
+mv /geoip/* /etc/nginx/geoip
+rm -rf /geoip
 
 # update image permissions
 writeDirs=( \
