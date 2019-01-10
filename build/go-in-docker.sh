d663f588792b3d2bf50262f27e840cc5d366dae4
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2019-01-09 21:13:32 -0300

    Update nginx image

--- build/go-in-docker.sh
+++ build/go-in-docker.sh
@@ -40,7 +40,7 @@ if [ "$missing" = true ];then
   exit 1
 fi
 
-E2E_IMAGE=quay.io/kubernetes-ingress-controller/e2e:v01042019-40487eea0
+E2E_IMAGE=quay.io/kubernetes-ingress-controller/e2e:v01092019-b433108ea
 
 DOCKER_OPTS=${DOCKER_OPTS:-""}
 
