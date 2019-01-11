83eaa67b6effecc6c3c041ebbc092e8e52d54ed6
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2019-01-11 14:47:43 -0300

    Change default value of  flag for ssl chain completion

--- cmd/nginx/flags.go
+++ cmd/nginx/flags.go
@@ -130,7 +130,7 @@ Requires the update-status parameter.`)
 		annotationsPrefix = flags.String("annotations-prefix", "nginx.ingress.kubernetes.io",
 			`Prefix of the Ingress annotations specific to the NGINX controller.`)
 
-		enableSSLChainCompletion = flags.Bool("enable-ssl-chain-completion", true,
+		enableSSLChainCompletion = flags.Bool("enable-ssl-chain-completion", false,
 			`Autocomplete SSL certificate chains with missing intermediate CA certificates.
 A valid certificate chain is required to enable OCSP stapling. Certificates
 uploaded to Kubernetes must have the "Authority Information Access" X.509 v3
