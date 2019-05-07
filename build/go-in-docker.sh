afd91d1a1c23bdca79770ef66534481752788e24
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2019-05-06 22:08:44 -0400

    Update go to 1.12.5, kubectl to 1.14.1 and kind to 0.2.1

--- build/go-in-docker.sh
+++ build/go-in-docker.sh
@@ -40,7 +40,7 @@ if [ "$missing" = true ];then
   exit 1
 fi
 
-E2E_IMAGE=quay.io/kubernetes-ingress-controller/e2e:v04172019-3fa3923cb
+E2E_IMAGE=quay.io/kubernetes-ingress-controller/e2e:v05062019-b4f2880ee
 
 DOCKER_OPTS=${DOCKER_OPTS:-""}
 
