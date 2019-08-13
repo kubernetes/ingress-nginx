be7acbb3fe4b723a6a19ef8b6063866f1c83e27e
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2019-08-13 16:08:11 -0400

    Fixes for CVE-2018-16843, CVE-2018-16844, CVE-2019-9511, CVE-2019-9513, and CVE-2019-9516

--- images/nginx/rootfs/build.sh
+++ images/nginx/rootfs/build.sh
@@ -376,6 +376,12 @@ Include /etc/nginx/owasp-modsecurity-crs/rules/RESPONSE-999-EXCLUSION-RULES-AFTE
 # build nginx
 cd "$BUILD_PATH/openresty-$OPENRESTY_VERSION"
 
+echo "Patching NGINX for CVE-2018-16843, CVE-2018-16844, CVE-2019-9511, CVE-2019-9513, and CVE-2019-9516"
+# Upstream change https://github.com/openresty/openresty/pull/515
+# TODO: remove after openresty release
+cat /patches/patch.2019.h2.txt | patch -d bundle/nginx-1.15.8/ -p0
+rm -rf /patches
+
 WITH_FLAGS="--with-debug \
   --with-compat \
   --with-pcre-jit \
