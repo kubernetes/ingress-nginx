ae67e33f990a7cbe822c2181b93a7d15fbeb786f
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2020-05-29 18:21:30 -0400

    Remove unused download of GeoIP databases

--- images/nginx/rootfs/build.sh
+++ images/nginx/rootfs/build.sh
@@ -113,22 +113,6 @@ apk add \
 
 mkdir -p /etc/nginx
 
-# Get the GeoIP data
-GEOIP_FOLDER=/etc/nginx/geoip
-mkdir -p $GEOIP_FOLDER
-
-function geoip2_get {
-  wget -O $GEOIP_FOLDER/$1.tar.gz $2 || { echo "Could not download $1, exiting." ; exit 1; }
-  mkdir $GEOIP_FOLDER/$1 \
-    && tar xf $GEOIP_FOLDER/$1.tar.gz -C $GEOIP_FOLDER/$1 --strip-components 1 \
-    && mv $GEOIP_FOLDER/$1/$1.mmdb $GEOIP_FOLDER/$1.mmdb \
-    && rm -rf $GEOIP_FOLDER/$1 \
-    && rm -rf $GEOIP_FOLDER/$1.tar.gz
-}
-
-#geoip2_get "GeoLite2-City"     "http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz"
-#geoip2_get "GeoLite2-ASN"      "http://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz"
-
 mkdir --verbose -p "$BUILD_PATH"
 cd "$BUILD_PATH"
 
