c73ddf30550e0953d909cac3ad28fa62c4c2da33
hao.yang01 <hao.yang01@wormpex.com>
2020-07-16 19:13:35 +0800

    updated

--- internal/ingress/types.go
+++ internal/ingress/types.go
@@ -123,8 +123,8 @@ type TrafficShapingPolicy struct {
 	HeaderPattern string `json:"headerPattern"`
 	// Cookie on which to redirect requests to this backend
 	Cookie string `json:"cookie"`
-	// HashHeader
-	// hash(the value of Header) % 100 Weight (0-100) of traffic to redirect to the backend
+	// HashHeader on which to redirect requests to this backend
+	// hash(the value of Header) % 100 ,Weight (0-100) of traffic to redirect to the backend
 	HashHeader string `json:"hashHeader"`
 	//HashHeaderWeight 20 means 20%
 	HashHeaderWeight int `json:"hashHeaderWeight"`
