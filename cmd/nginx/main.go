3afaf2df7ae3540af3ccfda5af3cd9e3a4e6105a
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2020-11-02 19:30:51 -0300

    Improve ingress class error message

--- cmd/nginx/main.go
+++ cmd/nginx/main.go
@@ -131,6 +131,7 @@ func main() {
 		}
 
 		if k8s.IngressClass != nil && k8s.IngressClass.Spec.Controller != k8s.IngressNGINXController {
+			klog.Errorf(`Invalid IngressClass (Spec.Controller) value "%v". Should be "%v"`, k8s.IngressClass.Spec.Controller, k8s.IngressNGINXController)
 			klog.Fatalf("IngressClass with name %v is not valid for ingress-nginx (invalid Spec.Controller)", class.IngressClass)
 		}
 	}
