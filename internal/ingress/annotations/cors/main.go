168a39e6c879f4567f0a18a36106e75fac009212
Ricardo Katz <rikatz@users.noreply.github.com>
2021-05-26 16:55:47 -0300

    Revert "feat: multiple-cors-allow-origin support (#7134)"
    This reverts commit 8a55801cc087e259493dc8ff787f3a37432c11a6.

--- internal/ingress/annotations/cors/main.go
+++ internal/ingress/annotations/cors/main.go
@@ -18,7 +18,6 @@ package cors
 
 import (
 	"regexp"
-	"strings"
 
 	networking "k8s.io/api/networking/v1beta1"
 
@@ -114,14 +113,7 @@ func (c cors) Parse(ing *networking.Ingress) (interface{}, error) {
 	}
 
 	config.CorsAllowOrigin, err = parser.GetStringAnnotation("cors-allow-origin", ing)
-	if err == nil {
-		for _, origin := range strings.Split(config.CorsAllowOrigin, ",") {
-			if !corsOriginRegex.MatchString(strings.TrimSpace(origin)) {
-				config.CorsAllowOrigin = "*"
-				break
-			}
-		}
-	} else {
+	if err != nil || !corsOriginRegex.MatchString(config.CorsAllowOrigin) {
 		config.CorsAllowOrigin = "*"
 	}
 
