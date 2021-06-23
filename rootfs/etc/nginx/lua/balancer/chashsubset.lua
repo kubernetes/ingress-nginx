48fe499fde80a4fd2b5bb29a20b0b843595fc3c4
Wei Wu <wei.wu@newsbreak.com>
2021-06-22 21:09:34 -0700

    use floor for buckets calculation to remove hot spot

--- rootfs/etc/nginx/lua/balancer/chashsubset.lua
+++ rootfs/etc/nginx/lua/balancer/chashsubset.lua
@@ -10,8 +10,23 @@ local tostring = tostring
 local math = math
 local table = table
 local pairs = pairs
+-- local string_format = string.format
 
 local _M = { name = "chashsubset" }
+
+local function dump(o)
+  -- Used for debug
+  if type(o) == 'table' then
+    local s = '{ '
+    for k,v in pairs(o) do
+      if type(k) ~= 'number' then k = '"'..k..'"' end
+      s = s .. '['..k..'] = ' .. dump(v) .. ','
+    end
+    return s .. '} '
+  else
+    return tostring(o)
+  end
+end
 
 local function build_subset_map(backend)
   local endpoints = {}
@@ -23,9 +38,11 @@ local function build_subset_map(backend)
     table.insert(endpoints, endpoint)
   end
 
-  local set_count = math.ceil(#endpoints/subset_size)
+  local set_count = math.floor(#endpoints/subset_size)
+  if set_count == 0 then
+    set_count = 1
+  end
   local node_count = set_count * subset_size
-
   -- if we don't have enough endpoints, we reuse endpoints in the last set to
   -- keep the same number on all of them.
   local j = 1
@@ -46,6 +63,7 @@ local function build_subset_map(backend)
     subset_map[subset_id] = 1
   end
 
+  --ngx_log(ngx_ERR, string_format("subsets: %s", dump(subsets)))
   return subset_map, subsets
 end
 
