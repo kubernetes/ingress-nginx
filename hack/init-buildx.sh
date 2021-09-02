cb8ebcb88022a4efdb90947930cc985e4261db99
Ricardo Katz <rikatz@users.noreply.github.com>
2021-09-02 15:59:39 -0300

    update alpine and remove buildx restriction (#7583)

--- hack/init-buildx.sh
+++ hack/init-buildx.sh
@@ -46,7 +46,9 @@ fi
 # We only need to do this setup on linux hosts
 if [ "$(uname)" == 'Linux' ]; then
   # NOTE: this is pinned to a digest for a reason!
-  docker run --rm --privileged multiarch/qemu-user-static@sha256:28ebe2e48220ae8fd5d04bb2c847293b24d7fbfad84f0b970246e0a4efd48ad6 --reset -p yes
+  # Note2 (@rikatz) - Removing the pin, as apparently it's breaking new alpine builds
+  # docker run --rm --privileged multiarch/qemu-user-static@sha256:28ebe2e48220ae8fd5d04bb2c847293b24d7fbfad84f0b970246e0a4efd48ad6 --reset -p yes
+  docker run --rm --privileged multiarch/qemu-user-static:register --reset -p yes
 fi
 
 # Ensure we use a builder that can leverage it (the default on linux will not)
