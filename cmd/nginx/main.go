9f1116c5472374732041f3d850f489de6cb1896a
Ricardo Katz <rikatz@users.noreply.github.com>
2021-10-10 20:18:37 -0300

    Add e2e test for non ingressclass enabled ingress (#7785)

--- cmd/nginx/main.go
+++ cmd/nginx/main.go
@@ -110,15 +110,12 @@ func main() {
 	_, err = kubeClient.NetworkingV1().IngressClasses().List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		if !errors.IsNotFound(err) {
-			if errors.IsUnauthorized(err) {
-				klog.Fatalf("Error searching IngressClass: Please verify your RBAC and allow Ingress Controller to list and get Ingress Classes: %v", err)
-			} else if errors.IsForbidden(err) {
+			if errors.IsForbidden(err) {
 				klog.Warningf("No permissions to list and get Ingress Classes: %v, IngressClass feature will be disabled", err)
 				conf.IngressClassConfiguration.IgnoreIngressClass = true
 			}
 		}
 	}
-
 	conf.Client = kubeClient
 
 	err = k8s.GetIngressPod(kubeClient)
