d2729ce2dd7edd9e5061994bdc6c0ca7e03aac86
Mateusz Gozdek <mgozdek@microsoft.com>
2021-10-12 12:41:11 +0200

    images/kube-webhook-certgen/rootfs: add missing tests implementation
    It should've been added in 9acf62d867145a071ad715020292bbb887289ba0.
    
    Signed-off-by: Mateusz Gozdek <mgozdek@microsoft.com>

--- images/kube-webhook-certgen/rootfs/pkg/k8s/k8s_test.go
+++ images/kube-webhook-certgen/rootfs/pkg/k8s/k8s_test.go
@@ -13,12 +13,15 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes/fake"
+	apiregistrationv1 "k8s.io/kube-aggregator/pkg/apis/apiregistration/v1"
+	aggregatorfake "k8s.io/kube-aggregator/pkg/client/clientset_generated/clientset/fake"
 )
 
 const (
-	testWebhookName = "c7c95710-d8c3-4cc3-a2a8-8d2b46909c76"
-	testSecretName  = "15906410-af2a-4f9b-8a2d-c08ffdd5e129"
-	testNamespace   = "7cad5f92-c0d5-4bc9-87a3-6f44d5a5619d"
+	testWebhookName    = "c7c95710-d8c3-4cc3-a2a8-8d2b46909c76"
+	testSecretName     = "15906410-af2a-4f9b-8a2d-c08ffdd5e129"
+	testAPIServiceName = "37f6a2d1-b401-4275-833b-9ff5004f0301"
+	testNamespace      = "7cad5f92-c0d5-4bc9-87a3-6f44d5a5619d"
 )
 
 var (
@@ -37,7 +40,8 @@ func genSecretData() (ca, cert, key []byte) {
 
 func newTestSimpleK8s(objects ...runtime.Object) *k8s {
 	return &k8s{
-		clientset: fake.NewSimpleClientset(objects...),
+		clientset:           fake.NewSimpleClientset(objects...),
+		aggregatorClientset: aggregatorfake.NewSimpleClientset(),
 	}
 }
 
@@ -162,37 +166,109 @@ func TestPatchWebhookConfigurations(t *testing.T) {
 func Test_Patching_objects(t *testing.T) {
 	t.Parallel()
 
+	ctx := contextWithDeadline(t)
+
 	t.Run("returns_error_when", func(t *testing.T) {
 		t.Parallel()
 
 		t.Run("failure_policy_is_defined_but_no_webhooks_will_be_patched", func(t *testing.T) {
 			t.Parallel()
+
+			k := testK8sWithUnpatchedObjects()
+
+			o := PatchOptions{
+				FailurePolicyType: admissionv1.Fail,
+			}
+
+			if err := k.PatchObjects(ctx, o); err == nil {
+				t.Fatalf("Expected error while patching")
+			}
 		})
 
 		// This is to preserve old behavior and log format, it could be improved.
-		t.Run("diffent_names_are_specified_for_validating_and_mutating_webhook", func(t *testing.T) {
+		t.Run("diffent_non_empty_names_are_specified_for_validating_and_mutating_webhook", func(t *testing.T) {
 			t.Parallel()
-		})
 
-		t.Run("patching_APIService_is_requested_and_it_does_not_exist", func(t *testing.T) {
-			t.Parallel()
+			k := testK8sWithUnpatchedObjects()
+
+			o := PatchOptions{
+				ValidatingWebhookConfigurationName: "foo",
+				MutatingWebhookConfigurationName:   "bar",
+			}
+
+			if err := k.PatchObjects(ctx, o); err == nil {
+				t.Fatalf("Expected error while patching")
+			}
 		})
 
 		t.Run("patching_webhook_is_requested_and_it_does_not_exist", func(t *testing.T) {
 			t.Parallel()
+
+			k := newTestSimpleK8s()
+
+			o := PatchOptions{
+				ValidatingWebhookConfigurationName: "foo",
+			}
+
+			if err := k.PatchObjects(ctx, o); err == nil {
+				t.Fatalf("Expected error while patching")
+			}
+		})
+
+		t.Run("patching_APIService_is_requested_and_it_does_not_exist", func(t *testing.T) {
+			t.Parallel()
+
+			k := newTestSimpleK8s()
+
+			o := PatchOptions{
+				APIServiceName: "foo",
+			}
+
+			if err := k.PatchObjects(ctx, o); err == nil {
+				t.Fatalf("Expected error while patching")
+			}
 		})
 	})
 
 	t.Run("when_patching_APIService_object", func(t *testing.T) {
 		t.Parallel()
 
+		k := testK8sWithUnpatchedObjects()
+
+		o := PatchOptions{
+			APIServiceName: testAPIServiceName,
+			CABundle:       []byte("foo"),
+		}
+
+		if err := k.PatchObjects(ctx, o); err != nil {
+			t.Fatalf("Unexpected error while patching objects: %v", err)
+		}
+
+		c := k.aggregatorClientset.ApiregistrationV1().APIServices()
+		apiService, err := c.Get(ctx, testAPIServiceName, metav1.GetOptions{})
+		if err != nil {
+			t.Fatalf("Unexpected error while getting APIService object %q: %v", testAPIServiceName, err)
+		}
+
 		// This is required when CABundle field is populated.
 		t.Run("sets_insecure_skip_tls_verity_to_false", func(t *testing.T) {
 			t.Parallel()
+
+			if apiService.Spec.InsecureSkipTLSVerify {
+				t.Fatalf("Expected insecureSkipTLSVerify of APIService to be false")
+			}
 		})
 
 		t.Run("sets_ca_bundle_with_ca_certificate_from_created_secret", func(t *testing.T) {
 			t.Parallel()
+
+			if len(apiService.Spec.CABundle) == 0 {
+				t.Fatalf("Expected CABundle of APIService to be not empty")
+			}
+
+			if !bytes.Equal(o.CABundle, apiService.Spec.CABundle) {
+				t.Fatalf("CABundle content of APIService does not match requested bundle")
+			}
 		})
 	})
 }
@@ -217,4 +293,39 @@ func contextWithDeadline(t *testing.T) context.Context {
 
 	return ctx
 }
+
+func testK8sWithUnpatchedObjects() *k8s {
+	ca, cert, key := genSecretData()
+
+	secret := &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      testSecretName,
+			Namespace: testNamespace,
+		},
+		Data: map[string][]byte{"ca": ca, "cert": cert, "key": key},
+	}
+
+	validatingWebhook := &admissionv1.ValidatingWebhookConfiguration{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: testWebhookName,
+		},
+		Webhooks: []admissionv1.ValidatingWebhook{{Name: "v1"}, {Name: "v2"}},
+	}
+	mutatingWebhook := &admissionv1.MutatingWebhookConfiguration{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: testWebhookName,
+		},
+		Webhooks: []admissionv1.MutatingWebhook{{Name: "m1"}, {Name: "m2"}},
+	}
+
+	apiService := &apiregistrationv1.APIService{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: testAPIServiceName,
+		},
+	}
+
+	return &k8s{
+		clientset:           fake.NewSimpleClientset(secret, validatingWebhook, mutatingWebhook),
+		aggregatorClientset: aggregatorfake.NewSimpleClientset(apiService),
+	}
 }
