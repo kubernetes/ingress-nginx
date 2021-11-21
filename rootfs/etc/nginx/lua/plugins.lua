f18cf99e62df3d3e585dfa44751fe82484d0b4fa
Renato Biassoni <r.biassoni@geniodiligence.it>
2021-11-21 22:21:32 +0100

    Use an indexed table to preserve order when loading plugins from names/paths

--- rootfs/etc/nginx/lua/plugins.lua
+++ rootfs/etc/nginx/lua/plugins.lua
@@ -12,7 +12,7 @@ local _M = {}
 local MAX_NUMBER_OF_PLUGINS = 20
 local plugins = {}
 
-local function load_plugin(name)
+local function load_plugin(i, name)
   local path = string_format("plugins.%s.main", name)
 
   local ok, plugin = pcall(require, path)
@@ -20,18 +20,17 @@ local function load_plugin(name)
     ngx_log(ERR, string_format("error loading plugin \"%s\": %s", path, plugin))
     return
   end
-
-  plugins[name] = plugin
+  plugins[i] = { name = name, plugin = plugin }
 end
 
 function _M.init(names)
   local count = 0
-  for _, name in ipairs(names) do
+  for i, name in ipairs(names) do
     if count >= MAX_NUMBER_OF_PLUGINS then
       ngx_log(ERR, "the total number of plugins exceed the maximum number: ", MAX_NUMBER_OF_PLUGINS)
       break
     end
-    load_plugin(name)
+    load_plugin(i, name)
     count = count + 1 -- ignore loading failure, just count the total
   end
 end
@@ -39,10 +38,11 @@ end
 function _M.run()
   local phase = ngx.get_phase()
 
-  for name, plugin in pairs(plugins) do
+  for _, plugin_module in ipairs(plugins) do
+    local name = plugin_module.name
+    local plugin = plugin_module.plugin
     if plugin[phase] then
       ngx_log(INFO, string_format("running plugin \"%s\" in phase \"%s\"", name, phase))
-
       -- TODO: consider sandboxing this, should we?
       -- probably yes, at least prohibit plugin from accessing env vars etc
       -- but since the plugins are going to be installed by ingress-nginx
