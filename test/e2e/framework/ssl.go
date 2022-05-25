347b7110883b6d761392e956b1e6260b3d946b06
Ricardo Pchevuzinske Katz <ricardo.katz@gmail.com>
2022-05-25 20:42:33 -0300

    fix algorithm

--- test/e2e/framework/ssl.go
+++ test/e2e/framework/ssl.go
@@ -170,9 +170,8 @@ func generateRSACert(host string, isCA bool, keyOut, certOut io.Writer) error {
 			CommonName:   "default",
 			Organization: []string{"Acme Co"},
 		},
-		SignatureAlgorithm: x509.ECDSAWithSHA1,
-		NotBefore:          notBefore,
-		NotAfter:           notAfter,
+		NotBefore: notBefore,
+		NotAfter:  notAfter,
 
 		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
