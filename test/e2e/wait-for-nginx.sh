840f00569921f48318b12359241e9c5c6db11b2e
Ehsan Saei <71217171+esigo@users.noreply.github.com>
2022-09-25 20:24:04 +0200

    mount otel module for otel test only

--- test/e2e/wait-for-nginx.sh
+++ test/e2e/wait-for-nginx.sh
@@ -46,6 +46,19 @@ metadata:
 
 EOF
 
+OTEL_MODULE=$(cat <<EOF
+  extraModules:
+    - name: opentelemetry
+      image: registry.k8s.io/ingress-nginx/opentelemetry:v20220906-g981ce38a7
+EOF
+)
+
+echo "${OTEL_MODULE}"
+
+if [[ "$NAMESPACE_OVERLAY" != "enable-opentelemetry" ]]; then
+  OTEL_MODULE=""
+fi
+
 # Use the namespace overlay if it was requested
 if [[ ! -z "$NAMESPACE_OVERLAY" && -d "$DIR/namespace-overlays/$NAMESPACE_OVERLAY" ]]; then
     echo "Namespace overlay $NAMESPACE_OVERLAY is being used for namespace $NAMESPACE"
@@ -100,9 +113,8 @@ controller:
       hostPath:
         path: /tmp/coredump
 
-  extraModules:
-    - name: opentelemetry
-      image: registry.k8s.io/ingress-nginx/opentelemetry:v20220906-g981ce38a7@sha256:aa079daa7efd93aa830e26483a49a6343354518360929494bad1d0ad3303142e
+
+${OTEL_MODULE}
 
 rbac:
   create: true
