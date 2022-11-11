71e9bdcb685acbfb14d3d7afa56189d7f0f8258f
James Strong <strong.james.e@gmail.com>
2022-11-11 09:23:19 -0500

    Add mage files for changelog
    Signed-off-by: James Strong <strong.james.e@gmail.com>

---
+++ magefiles/go.go
@@ -0,0 +1,15 @@
+//go:build mage
+
+package main
+
+//import (
+//	"github.com/magefile/mage/mg"
+//	"github.com/mysteriumnetwork/go-ci/commands"
+//)
+//
+//type Go mg.Namespace
+//
+//// Checks for issues with go imports
+//func (Go) CheckGoImports() error {
+//	return commands.GoImports("./...")
+//}
