96c7f2881665acee1f780e298e6d2140094e77fb
Ricardo Katz <rikatz@users.noreply.github.com>
2022-11-17 09:24:40 -0300

    Validate ingress path fields  (#9309)
    * Validate characters in path fields
    
    * Add e2e tests for path validation
    
    * Fix review comments

--- internal/ingress/controller/store/store.go
+++ internal/ingress/controller/store/store.go
@@ -59,6 +59,7 @@ import (
 	"k8s.io/ingress-nginx/internal/ingress/resolver"
 	"k8s.io/ingress-nginx/internal/k8s"
 	"k8s.io/ingress-nginx/pkg/apis/ingress"
+	ingressutils "k8s.io/ingress-nginx/pkg/util/ingress"
 )
 
 // IngressFilterFunc decides if an Ingress should be omitted or not
@@ -860,6 +861,10 @@ func (s *k8sStore) syncIngress(ing *networkingv1.Ingress) {
 		for pi, path := range rule.HTTP.Paths {
 			if path.Path == "" {
 				copyIng.Spec.Rules[ri].HTTP.Paths[pi].Path = "/"
+			}
+			if !ingressutils.IsSafePath(copyIng, path.Path) {
+				klog.Warningf("ingress %s contains invalid path %s", key, path.Path)
+				return
 			}
 		}
 	}
