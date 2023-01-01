45f1defdc6d14408b36e79e8ce7ed27e97b28d90
João Henri <joao.henri.cr@gmail.com>
2023-01-01 19:12:41 -0300

    Add executable that contains Go's symbol table

--- build/build.sh
+++ build/build.sh
@@ -57,6 +57,16 @@ ${GO_BUILD_CMD} \
   -buildvcs=false \
   -o "${TARGETS_DIR}/nginx-ingress-controller" "${PKG}/cmd/nginx"
 
+echo "Building ${PKG}/cmd/nginx with Go's symbol table"
+
+${GO_BUILD_CMD} \
+  -trimpath -ldflags="-buildid= -w \
+  -X ${PKG}/version.RELEASE=${TAG} \
+  -X ${PKG}/version.COMMIT=${COMMIT_SHA} \
+  -X ${PKG}/version.REPO=${REPO_INFO}" \
+  -buildvcs=false \
+  -o "${TARGETS_DIR}/scannable-nginx-ingress-controller" "${PKG}/cmd/nginx"
+
 echo "Building ${PKG}/cmd/dbg"
 
 ${GO_BUILD_CMD} \
