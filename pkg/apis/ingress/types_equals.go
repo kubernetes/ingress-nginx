2e2831875690f010c5596155040a22e16839b125
Fish-pro <zechun.chen@daocloud.io>
2023-02-16 21:59:39 +0800

    Optimize the judgment mode to remove redundant transformations (#9588)
    Signed-off-by: Fish-pro <zechun.chen@daocloud.io>

--- pkg/apis/ingress/types_equals.go
+++ pkg/apis/ingress/types_equals.go
@@ -76,11 +76,7 @@ func (c1 *Configuration) Equal(c2 *Configuration) bool {
 		}
 	}
 
-	if c1.BackendConfigChecksum != c2.BackendConfigChecksum {
-		return false
-	}
-
-	return true
+	return c1.BackendConfigChecksum == c2.BackendConfigChecksum
 }
 
 // Equal tests for equality between two Backend types
