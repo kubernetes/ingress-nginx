87f27d98ab7403cb6ae72a15b2ba365b8ef91133
Christian Groschupp <christian@groschupp.org>
2023-03-15 14:14:27 +0100

    add custom headers
    Signed-off-by: Christian Groschupp <christian@groschupp.org>

--- internal/ingress/controller/controller.go
+++ internal/ingress/controller/controller.go
@@ -1504,6 +1504,7 @@ func (n *NGINXController) createServers(data []*ingress.Ingress,
 func locationApplyAnnotations(loc *ingress.Location, anns *annotations.Ingress) {
 	loc.BasicDigestAuth = anns.BasicDigestAuth
 	loc.ClientBodyBufferSize = anns.ClientBodyBufferSize
+	loc.CustomHeaders = anns.CustomHeaders
 	loc.ConfigurationSnippet = anns.ConfigurationSnippet
 	loc.CorsConfig = anns.CorsConfig
 	loc.ExternalAuth = anns.ExternalAuth
