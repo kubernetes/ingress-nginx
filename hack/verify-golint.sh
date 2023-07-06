facf582069a07663b7c0bccf15b5dba7e05649c3
z1cheng <imchench@gmail.com>
2023-07-06 12:29:48 +0000

    Add golangci github action and replace the deprecated golint
    Signed-off-by: z1cheng <imchench@gmail.com>

--- hack/verify-golint.sh
+++ hack/verify-golint.sh
@@ -22,19 +22,6 @@ KUBE_ROOT=$(dirname "${BASH_SOURCE}")/..
 
 cd "${KUBE_ROOT}"
 
-GOLINT=${GOLINT:-"golint"}
-PACKAGES=($(go list ./internal/... | grep -v /vendor/))
-bad_files=()
-for package in "${PACKAGES[@]}"; do
-  out=$("${GOLINT}" -min_confidence=0.9 "${package}" | grep -v -E '(should not use dot imports|internal/file/bindata.go)' || :)
-  if [[ -n "${out}" ]]; then
-    bad_files+=("${out}")
-  fi
-done
-if [[ "${#bad_files[@]}" -ne 0 ]]; then
-  echo "!!! '$GOLINT' problems: "
-  echo "${bad_files[@]}"
-  exit 1
-fi
+LINT=${LINT:-golangci-lint}
 
-# ex: ts=2 sw=2 et filetype=sh
+${LINT} run
