71dc6447ad38072cbdb6a6e50be60b8f0a52e942
Nicholas Orlowsky <nickorlow@nickorlow.com>
2023-07-16 23:53:42 -0400

    remove build flags and rename cgroups_linux.go

--- test/e2e/e2e.go
+++ test/e2e/e2e.go
@@ -53,6 +53,7 @@ import (
 	_ "k8s.io/ingress-nginx/test/e2e/ssl"
 	_ "k8s.io/ingress-nginx/test/e2e/status"
 	_ "k8s.io/ingress-nginx/test/e2e/tcpudp"
+	_ "k8s.io/ingress-nginx/test/e2e/cgroups"
 )
 
 // RunE2ETests checks configuration parameters (specified through flags) and then runs
