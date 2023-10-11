ee82a126e332f129e0de2662c41f947479b75349
k8s-infra-cherrypick-robot <90416843+k8s-infra-cherrypick-robot@users.noreply.github.com>
2023-10-11 16:02:59 -0700

    [release-1.9] update nginx base, httpbun, e2e, helm webhook cert gen (#10507)
    * update nginx base, httpbun, e2e, helm webhook cert gen
    
    Signed-off-by: James Strong <strong.james.e@gmail.com>
    
    * fix helm docs
    
    Signed-off-by: James Strong <strong.james.e@gmail.com>
    
    ---------
    
    Signed-off-by: James Strong <strong.james.e@gmail.com>
    Co-authored-by: James Strong <strong.james.e@gmail.com>

--- build/run-in-docker.sh
+++ build/run-in-docker.sh
@@ -44,7 +44,7 @@ function cleanup {
 }
 trap cleanup EXIT
 
-E2E_IMAGE=${E2E_IMAGE:-registry.k8s.io/ingress-nginx/e2e-test-runner:v20230907-5bb82dcb7@sha256:421cda0f65a949b8b67b5e62a45071702d19ed458a3e2ba753171b0e66943210}
+E2E_IMAGE=${E2E_IMAGE:-registry.k8s.io/ingress-nginx/e2e-test-runner:v20231011-8b53cabe0@sha256:ed0dad805c635e66469b4ac376010eebdd0b3fe62d753f58db1632d6f12f451d}
 
 if [[ "$RUNTIME" == podman ]]; then
   # Podman does not support both tag and digest
