7100fecafb07296e38086e79ee5bbe7d7f6cc825
k8s-infra-cherrypick-robot <90416843+k8s-infra-cherrypick-robot@users.noreply.github.com>
2023-12-01 08:25:56 -0800

    fix: remove tcpproxy copy error handling (#10715)
    Co-authored-by: Zadkiel Aharonian <hello@zadkiel.fr>

--- pkg/tcpproxy/tcp.go
+++ pkg/tcpproxy/tcp.go
@@ -125,9 +125,8 @@ func (p *TCPProxy) Handle(conn net.Conn) {
 
 func pipe(client, server net.Conn) {
 	doCopy := func(s, c net.Conn, cancel chan<- bool) {
-		if _, err := io.Copy(s, c); err != nil {
-			klog.Errorf("Error copying data: %v", err)
-		}
+		//nolint:errcheck // No need to catch these errors
+		io.Copy(s, c)
 		cancel <- true
 	}
 
