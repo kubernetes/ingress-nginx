e9664c2566810b316d857250439fcb990328741f
Fabián Sellés Rosa <fabian.sellesrosa@gmail.com>
2024-01-30 00:01:03 +0100

    Add disable path overlap validation flag
      ## What this PR does / why we need it:
    
    In https://github.com/kubernetes/ingress-nginx/issues/5651 there was a
    request to throw an error when there are two ingresses defining the same
    host and path, which was implemented as part of the validation webhook.
    
      Despite of this there are clear rules on the ingress controller that
    describes what the controller would do in [such situation (the oldest
    rule wins)](https://github.com/kubernetes/ingress-nginx/blob/main/docs/how-it-works.md?plain=1#L27)
    
      Some users are relying on this validation behaviour to prevent
    misconfigurations, but there are use cases where allowing it, maybe
    temporarily, is helpful. Those use cases includes:
    
      - Splitting large ingresses objects in smaller ones https://github.com/kubernetes/ingress-nginx/issues/10820
      - Moving ingress objects between namespaces without downtime (like when you transfer an ingress from team A that owns namespace A to team B that owns namespace B) https://github.com/kubernetes/ingress-nginx/issues/10090
    
    <!--- What types of changes does your code introduce? Put an `x` in all the boxes that apply: -->
    
      ## Types of changes
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [X] New feature (non-breaking change which adds functionality)
    - [ ] CVE Report (Scanner found CVE and adding report)
    - [ ] Breaking change (fix or feature that would cause existing functionality to change)
    - [ ] Documentation only
    
      ## Which issue/s this PR fixes
    
    It might help with #10820
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
      ## How Has This Been Tested?
    
    building an image and testing it in a local cluster, will update later
    with some real life scenarios
    
    <!--- Go over all the following points, and put an `x` in all the boxes that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're here to help! -->
      ## Checklist:
    
    - [X] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [X] I've read the [CONTRIBUTION](https://github.com/kubernetes/ingress-nginx/blob/main/CONTRIBUTING.md) guide
    - [X] I have added unit and/or e2e tests to cover my changes.
    - [X] All new and existing tests passed.
    
    Change-Id: I9d4124d1c36876b06d63100cd10988eaf2f41db9

--- internal/ingress/controller/controller.go
+++ internal/ingress/controller/controller.go
@@ -122,10 +122,11 @@ type Configuration struct {
 
 	IngressClassConfiguration *ingressclass.Configuration
 
-	ValidationWebhook         string
-	ValidationWebhookCertPath string
-	ValidationWebhookKeyPath  string
-	DisableFullValidationTest bool
+	ValidationWebhook            string
+	ValidationWebhookCertPath    string
+	ValidationWebhookKeyPath     string
+	DisableFullValidationTest    bool
+	DisablePathOverlapValidation bool
 
 	GlobalExternalAuth  *ngx_config.GlobalExternalAuth
 	MaxmindEditionFiles *[]string
@@ -403,10 +404,14 @@ func (n *NGINXController) CheckIngress(ing *networking.Ingress) error {
 	startTest := time.Now().UnixNano() / 1000000
 	_, servers, pcfg := n.getConfiguration(ings)
 
-	err = checkOverlap(ing, servers)
-	if err != nil {
-		n.metricCollector.IncCheckErrorCount(ing.ObjectMeta.Namespace, ing.Name)
-		return err
+	if n.cfg.DisablePathOverlapValidation {
+		klog.Warningf("ingress %v in namespace %v not checked for path overlap since --disable-path-overlap-validation is enabled", ing.Name, ing.ObjectMeta.Namespace)
+	} else {
+		err = checkOverlap(ing, servers)
+		if err != nil {
+			n.metricCollector.IncCheckErrorCount(ing.ObjectMeta.Namespace, ing.Name)
+			return err
+		}
 	}
 	testedSize := len(ings)
 	if n.cfg.DisableFullValidationTest {
