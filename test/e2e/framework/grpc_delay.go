fbf77e3ef1d11b76e12008bae29d0042c7d47c70
Anddd7 <anddd7@icloud.com>
2024-04-25 21:11:32 +0800

    🐛 fix: lint

--- test/e2e/framework/grpc_delay.go
+++ test/e2e/framework/grpc_delay.go
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-//nolint:dupl // Ignore dupl errors for similar test case
 package framework
 
 import (
