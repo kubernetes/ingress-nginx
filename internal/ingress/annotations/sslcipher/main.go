bdcbeb63547c7d620f0e0a77b4cfacd1c6fbb6eb
longwuyuan <longwuyuan@gmail.com>
2024-05-04 16:06:34 +0530

    removed tlsv1 & tlsv1.1

--- internal/ingress/annotations/sslcipher/main.go
+++ internal/ingress/annotations/sslcipher/main.go
@@ -43,7 +43,7 @@ var sslCipherAnnotations = parser.Annotation{
 			Scope:     parser.AnnotationScopeIngress,
 			Risk:      parser.AnnotationRiskLow,
 			Documentation: `The following annotation will set the ssl_prefer_server_ciphers directive at the server level. 
-			This configuration specifies that server ciphers should be preferred over client ciphers when using the SSLv3 and TLS protocols.`,
+			This configuration specifies that server ciphers should be preferred over client ciphers when using the TLS protocols.`,
 		},
 		sslCipherAnnotation: {
 			Validator:     parser.ValidateRegex(regexValidSSLCipher, true),
