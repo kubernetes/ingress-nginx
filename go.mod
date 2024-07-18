a1d0c4c01150dd01556a80c77066615fecfd8d25
Ricardo Katz <ricardo.katz@gmail.com>
2024-07-18 19:21:03 +0000

    Bootstrap initial crossplane work

--- go.mod
+++ go.mod
@@ -48,6 +48,12 @@ require (
 )
 
 require (
+	github.com/jstemmer/go-junit-report v1.0.0 // indirect
+	github.com/maxbrunsfeld/counterfeiter/v6 v6.8.1 // indirect
+	github.com/nginxinc/nginx-go-crossplane v0.4.50 // indirect
+)
+
+require (
 	github.com/Anddd7/pb v0.0.0-20240425032658-369b0f6a404c
 	github.com/Azure/go-ansiterm v0.0.0-20230124172434-306776ec8161 // indirect
 	github.com/BurntSushi/toml v1.3.2 // indirect
@@ -108,15 +114,15 @@ require (
 	github.com/yudai/pp v2.0.1+incompatible // indirect
 	go.starlark.net v0.0.0-20240123142251-f86470692795 // indirect
 	go.uber.org/zap v1.27.0 // indirect
-	golang.org/x/mod v0.17.0 // indirect
-	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/mod v0.19.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
 	golang.org/x/oauth2 v0.21.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.22.0 // indirect
 	golang.org/x/term v0.22.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
-	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
+	golang.org/x/tools v0.23.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240528184218-531527333157 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/evanphx/json-patch.v5 v5.9.0 // indirect
---
+++ internal/ingress/controller/template/crossplane/utils.go
@@ -0,0 +1,100 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package crossplane
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+
+	ngx_crossplane "github.com/nginxinc/nginx-go-crossplane"
+
+	"k8s.io/ingress-nginx/internal/ingress/controller/config"
+	ing_net "k8s.io/ingress-nginx/internal/net"
+)
+
+type seconds int
+
+func buildDirective(directive string, args ...any) *ngx_crossplane.Directive {
+	argsVal := make([]string, 0)
+	for k := range args {
+		switch v := args[k].(type) {
+		case string:
+			argsVal = append(argsVal, v)
+		case []string:
+			argsVal = append(argsVal, v...)
+		case int:
+			argsVal = append(argsVal, strconv.Itoa(v))
+		case bool:
+			argsVal = append(argsVal, boolToStr(v))
+		case seconds:
+			argsVal = append(argsVal, strconv.Itoa(int(v))+"s")
+		}
+	}
+	return &ngx_crossplane.Directive{
+		Directive: directive,
+		Args:      argsVal,
+	}
+}
+
+func buildLuaSharedDictionaries(cfg config.Configuration) []*ngx_crossplane.Directive {
+	out := make([]*ngx_crossplane.Directive, 0, len(cfg.LuaSharedDicts))
+	for name, size := range cfg.LuaSharedDicts {
+		sizeStr := dictKbToStr(size)
+		out = append(out, buildDirective("lua_shared_dict", name, sizeStr))
+	}
+
+	return out
+}
+
+// TODO: The utils below should be moved to a level where they can be consumed by any template writer
+
+// buildResolvers returns the resolvers reading the /etc/resolv.conf file
+func buildResolversInternal(res []net.IP, disableIpv6 bool) []string {
+	r := make([]string, 0)
+	for _, ns := range res {
+		if ing_net.IsIPV6(ns) {
+			if disableIpv6 {
+				continue
+			}
+			r = append(r, fmt.Sprintf("[%s]", ns))
+		} else {
+			r = append(r, ns.String())
+		}
+	}
+	r = append(r, "valid=30s")
+
+	if disableIpv6 {
+		r = append(r, "ipv6=off")
+	}
+
+	return r
+}
+
+func boolToStr(b bool) string {
+	if b {
+		return "on"
+	}
+	return "off"
+}
+
+func dictKbToStr(size int) string {
+	if size%1024 == 0 {
+		return fmt.Sprintf("%dM", size/1024)
+	}
+	return fmt.Sprintf("%dK", size)
+}
