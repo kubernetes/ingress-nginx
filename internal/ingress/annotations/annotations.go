55d8dbfdacd7d31cf39206b58d04f660582f4f07
Johannes Rudolph <johannes.rudolph@gmail.com>
2024-08-06 14:16:16 +0200

    alternatively just create an event if an annotation was denied

--- internal/ingress/annotations/annotations.go
+++ internal/ingress/annotations/annotations.go
@@ -212,8 +212,7 @@ func (e Extractor) Extract(ing *networking.Ingress) (*Ingress, error) {
 				val = nil
 			}
 
-			return nil, err
-			/* _, alreadyDenied := data[DeniedKeyName]
+			_, alreadyDenied := data[DeniedKeyName]
 			if !alreadyDenied {
 				errString := err.Error()
 				data[DeniedKeyName] = &errString
@@ -221,7 +220,7 @@ func (e Extractor) Extract(ing *networking.Ingress) (*Ingress, error) {
 				continue
 			}
 
-			klog.V(5).ErrorS(err, "error reading Ingress annotation", "name", name, "ingress", klog.KObj(ing)) */
+			klog.V(5).ErrorS(err, "error reading Ingress annotation", "name", name, "ingress", klog.KObj(ing))
 		}
 
 		if val != nil {
