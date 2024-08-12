b68dad762c6c98dc5ce0816ce3d3473a49fdd10b
Marco Ebert <marco_ebert@icloud.com>
2024-08-13 01:28:56 +0200

    Tests: Bump `e2e-test-runner` to v20240812-3f0129aa.

--- test/e2e/run-chart-test.sh
+++ test/e2e/run-chart-test.sh
@@ -109,7 +109,7 @@ docker run --rm --interactive --network host \
     --volume $KUBECONFIG:/root/.kube/config \
     --volume "${DIR}/../../":/workdir \
     --workdir /workdir \
-    registry.k8s.io/ingress-nginx/e2e-test-runner:v20240729-04899b27@sha256:9f067e6c861343da47f5851bf255aecdeb33a61fc5991a6c659fb237a3f619f9 \
+    registry.k8s.io/ingress-nginx/e2e-test-runner:v20240812-3f0129aa@sha256:95c2aaf2a66e8cbbf7a7453046f3b024383c273a0988efab841cd96116afd1a9 \
         ct install \
         --charts charts/ingress-nginx \
         --helm-extra-args "--timeout 60s"
