78d8d8a927b961500f14789aabaa38fa7355f48c
Marco Ebert <marco_ebert@icloud.com>
2024-08-29 13:37:47 +0200

    Bump github.com/onsi/ginkgo/v2 from 2.20.0 to 2.20.2 elsewhere

--- test/e2e/run-kind-e2e.sh
+++ test/e2e/run-kind-e2e.sh
@@ -96,7 +96,7 @@ fi
 
 if [ "${SKIP_E2E_IMAGE_CREATION}" = "false" ]; then
   if ! command -v ginkgo &> /dev/null; then
-    go install github.com/onsi/ginkgo/v2/ginkgo@v2.20.0
+    go install github.com/onsi/ginkgo/v2/ginkgo@v2.20.2
   fi
 
   echo "[dev-env] .. done building controller images"
