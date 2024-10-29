926b757c97db9bcd041faaecd87ecf1b3bbbcff9
Ricardo Katz <ricardo.katz@gmail.com>
2024-10-29 09:04:36 -0300

    Fix lua lint

--- rootfs/etc/nginx/lua/nginx/ngx_conf_external_auth.lua
+++ rootfs/etc/nginx/lua/nginx/ngx_conf_external_auth.lua
@@ -6,7 +6,9 @@ local ipairs = ipairs
 local ngx_log = ngx.log
 local ngx_ERR = ngx.ERR
 
-local res = ngx.location.capture(auth_path, { method = ngx.HTTP_GET, body = '', share_all_vars = auth_keepalive_share_vars })
+local res = ngx.location.capture(auth_path, {
+    method = ngx.HTTP_GET, body = '',
+    share_all_vars = auth_keepalive_share_vars })
 if res.status == ngx.HTTP_OK then
     local header_parts, err = ngx_re_split(auth_response_headers, ",")
     if err then
