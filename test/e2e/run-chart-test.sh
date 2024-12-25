efa41b7aaf60e386bc53f4b1343f37af7948d1d4
Marco Ebert <marco_ebert@icloud.com>
2024-12-25 02:11:18 +0100

    Tests: Bump `e2e-test-runner` to v20241224-68ed4e7b. (#12589)

--- test/e2e/run-chart-test.sh
+++ test/e2e/run-chart-test.sh
@@ -114,5 +114,5 @@ docker run \
   --workdir /workdir \
   --entrypoint ct \
   --rm \
-  registry.k8s.io/ingress-nginx/e2e-test-runner:v20241104-02a3933e@sha256:baf30e414c5657cc71f5bd1db502f0e0ee4ab721b6560340eff214935e96bef0 \
+  registry.k8s.io/ingress-nginx/e2e-test-runner:v20241224-68ed4e7b@sha256:871642296ebc0dd386f9a43b0cf2606028d757d6c4a2737d41180f02f8172823 \
     install --charts charts/ingress-nginx
