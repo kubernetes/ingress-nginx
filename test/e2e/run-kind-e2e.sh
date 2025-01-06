15087c9d9deb8f7bbeb62ffa3797971aaeb2aa7c
k8s-infra-cherrypick-robot <90416843+k8s-infra-cherrypick-robot@users.noreply.github.com>
2025-01-06 12:42:31 -0800

    Bump github.com/onsi/ginkgo/v2 from 2.22.1 to 2.22.2 (#12630)
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- test/e2e/run-kind-e2e.sh
+++ test/e2e/run-kind-e2e.sh
@@ -96,7 +96,7 @@ fi
 
 if [ "${SKIP_E2E_IMAGE_CREATION}" = "false" ]; then
   if ! command -v ginkgo &> /dev/null; then
-    go install github.com/onsi/ginkgo/v2/ginkgo@v2.22.1
+    go install github.com/onsi/ginkgo/v2/ginkgo@v2.22.2
   fi
 
   echo "[dev-env] .. done building controller images"
