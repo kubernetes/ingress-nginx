b24bc7aff4d42df5e00908f279286b0486b46088
k8s-infra-cherrypick-robot <90416843+k8s-infra-cherrypick-robot@users.noreply.github.com>
2025-03-24 11:39:17 -0700

    Tests & Docs: Bump images. (#13064)
    Co-authored-by: Marco Ebert <marco_ebert@icloud.com>

--- test/e2e/framework/fastcgi_helloserver.go
+++ test/e2e/framework/fastcgi_helloserver.go
@@ -59,7 +59,7 @@ func (f *Framework) NewNewFastCGIHelloServerDeploymentWithReplicas(replicas int3
 					Containers: []corev1.Container{
 						{
 							Name:  "fastcgi-helloserver",
-							Image: "registry.k8s.io/ingress-nginx/fastcgi-helloserver:v1.1.1@sha256:6af4d8c7745c6727aab759db616a58fd68d784d07ce7a32d1ad149c331fd9a6f",
+							Image: "registry.k8s.io/ingress-nginx/fastcgi-helloserver:v1.1.2@sha256:67133b24b7a9f556aa6977484db5af7e16f6a9509dba757cc2c003834c0af489",
 							Env:   []corev1.EnvVar{},
 							Ports: []corev1.ContainerPort{
 								{
