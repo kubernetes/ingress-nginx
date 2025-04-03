61f317f1c1aa29ab49afd7b05ca4e264ad6eb64e
Neer Friedman <neerfri@gmail.com>
2025-04-03 15:33:36 +0300

    making test fail on purpose to check if tests work properly

--- test/e2e/servicebackend/service_externalname.go
+++ test/e2e/servicebackend/service_externalname.go
@@ -142,7 +142,7 @@ var _ = framework.IngressNginxDescribe("[Service] Type ExternalName", func() {
 			GET("/get").
 			WithHeader("Host", host).
 			Expect().
-			Status(http.StatusOK)
+			Status(http.StatusCreated)
 	})
 
 	ginkgo.It("should return status 502 for service type=ExternalName with an invalid host", func() {
