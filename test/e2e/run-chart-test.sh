243815060a6ee9880c04333f4a12fd00c70bc793
Marco Ebert <marco_ebert@icloud.com>
2025-05-21 16:02:04 +0200

    CI: Update Kubernetes.

--- test/e2e/run-chart-test.sh
+++ test/e2e/run-chart-test.sh
@@ -62,7 +62,7 @@ export KUBECONFIG="${KUBECONFIG:-$HOME/.kube/kind-config-$KIND_CLUSTER_NAME}"
 if [ "${SKIP_CLUSTER_CREATION:-false}" = "false" ]; then
   echo "[dev-env] creating Kubernetes cluster with kind"
 
-  export K8S_VERSION=${K8S_VERSION:-v1.32.3@sha256:b36e76b4ad37b88539ce5e07425f77b29f73a8eaaebf3f1a8bc9c764401d118c}
+  export K8S_VERSION=${K8S_VERSION:-v1.33.1@sha256:050072256b9a903bd914c0b2866828150cb229cea0efe5892e2b644d5dd3b34f}
 
   kind create cluster \
     --verbosity=${KIND_LOG_LEVEL} \
