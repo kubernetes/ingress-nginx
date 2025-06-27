9d8dae6cb2dbfcbbbf74fe30f652b930df7c38a2
Marco Ebert <marco_ebert@icloud.com>
2025-06-27 17:14:11 +0200

    Tests: Bump Test Runner to v1.4.0.

--- build/run-in-docker.sh
+++ build/run-in-docker.sh
@@ -41,7 +41,7 @@ function cleanup {
 }
 trap cleanup EXIT
 
-E2E_IMAGE=${E2E_IMAGE:-registry.k8s.io/ingress-nginx/e2e-test-runner:v1.3.5@sha256:afef5aa6931be47d1c35f4de540c153032bbc26aefd44bc182f0bd3bb079f818}
+E2E_IMAGE=${E2E_IMAGE:-registry.k8s.io/ingress-nginx/e2e-test-runner:v1.4.0@sha256:ba366ecc1a67022d4f02193faf7af284fa7642e7a55f3eef1e52069c9fe306f0}
 
 if [[ "$RUNTIME" == podman ]]; then
   # Podman does not support both tag and digest
