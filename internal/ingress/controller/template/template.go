df74d102f22c66a4910faa833aeb1076cb5bb2fd
marcel2012 <13705842+marcel2012@users.noreply.github.com>
2025-07-20 21:18:00 +0200

    HTTP/3 generate QUIC listen configuration block

--- internal/ingress/controller/template/template.go
+++ internal/ingress/controller/template/template.go
@@ -1509,6 +1509,30 @@ func httpsListener(addresses []string, co string, tc *config.TemplateConfig) []s
 
 		out = append(out, strings.Join(lo, " "))
 	}
+	if !tc.Cfg.UseHTTP3 {
+		return out
+	}
+	if strings.Contains(co, "backlog=") {
+		klog.V(3).InfoS("Skipping HTTP/3 because of incompatible backlog parameter")
+		return out
+	}
+	if tc.IsSSLPassthroughEnabled {
+		klog.V(3).InfoS("Skipping HTTP/3 in SSL Passthrough mode")
+		return out
+	}
+	for _, address := range addresses {
+		lo := []string{"listen"}
+
+		if address == "" {
+			lo = append(lo, fmt.Sprintf("%v", tc.ListenPorts.QUIC))
+		} else {
+			lo = append(lo, fmt.Sprintf("%v:%v", address, tc.ListenPorts.QUIC))
+		}
+
+		lo = append(lo, co, "quic;")
+
+		out = append(out, strings.Join(lo, " "))
+	}
 
 	return out
 }
