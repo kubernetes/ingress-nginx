58ce1da1d58ff2dc283dc4a9954722576161208d
marcel2012 <13705842+marcel2012@users.noreply.github.com>
2025-08-30 17:40:27 +0200

    CI: Update KIND and Kubernetes to v1.34.0.

--- build/dev-env.sh
+++ build/dev-env.sh
@@ -64,7 +64,7 @@ echo "[dev-env] building image"
 make build image
 docker tag "${REGISTRY}/controller:${TAG}" "${DEV_IMAGE}"
 
-export K8S_VERSION=${K8S_VERSION:-v1.33.2@sha256:c55080dc5be4f2cc242e6966fdf97bb62282e1cd818a28223cf536db8b0fddf4}
+export K8S_VERSION=${K8S_VERSION:-v1.34.0@sha256:7416a61b42b1662ca6ca89f02028ac133a309a2a30ba309614e8ec94d976dc5a}
 
 KIND_CLUSTER_NAME="ingress-nginx-dev"
 
