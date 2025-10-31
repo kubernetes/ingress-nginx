2f2027c3bf119546fd750689fc6226f862d0fb15
k8s-infra-cherrypick-robot <90416843+k8s-infra-cherrypick-robot@users.noreply.github.com>
2025-10-31 04:20:02 -0700

    Annotations: Respect changes to `auth-proxy-set-headers`. (#14105)
    Co-authored-by: Anders Søe <anders@soee.org>

--- internal/ingress/annotations/authreq/main.go
+++ internal/ingress/annotations/authreq/main.go
@@ -18,6 +18,7 @@ package authreq
 
 import (
 	"fmt"
+	"reflect"
 	"regexp"
 	"strings"
 
@@ -240,6 +241,10 @@ func (e1 *Config) Equal(e2 *Config) bool {
 	}
 
 	if e1.AlwaysSetCookie != e2.AlwaysSetCookie {
+		return false
+	}
+
+	if !reflect.DeepEqual(e1.ProxySetHeaders, e2.ProxySetHeaders) {
 		return false
 	}
 
