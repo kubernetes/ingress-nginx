d90255057028c9e3ddbb82d6d624331fbec67e36
Marco Ebert <marco_ebert@icloud.com>
2025-11-05 00:32:32 +0100

    Go: Update dependencies.

--- test/e2e/run-kind-e2e.sh
+++ test/e2e/run-kind-e2e.sh
@@ -95,7 +95,7 @@ fi
 
 if [ "${SKIP_E2E_IMAGE_CREATION}" = "false" ]; then
   if ! command -v ginkgo &> /dev/null; then
-    go install github.com/onsi/ginkgo/v2/ginkgo@v2.27.1
+    go install github.com/onsi/ginkgo/v2/ginkgo@v2.27.2
   fi
 
   echo "[dev-env] .. done building controller images"
