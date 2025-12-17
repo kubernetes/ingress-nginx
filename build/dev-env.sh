1786122e2e75dae63624ae2ad8e8aa5e48a7bc95
Marco Ebert <marco_ebert@icloud.com>
2025-12-17 15:01:01 +0100

    CI: Update KIND to v1.34.3.

--- build/dev-env.sh
+++ build/dev-env.sh
@@ -64,7 +64,7 @@ echo "[dev-env] building image"
 make build image
 docker tag "${REGISTRY}/controller:${TAG}" "${DEV_IMAGE}"
 
-export K8S_VERSION=${K8S_VERSION:-v1.34.2@sha256:745f8ed46d8e99517774768227fd1a0af34a6bf395aef9c7ed98fbce0e263918}
+export K8S_VERSION=${K8S_VERSION:-v1.34.3@sha256:08497ee19eace7b4b5348db5c6a1591d7752b164530a36f855cb0f2bdcbadd48}
 
 KIND_CLUSTER_NAME="ingress-nginx-dev"
 
