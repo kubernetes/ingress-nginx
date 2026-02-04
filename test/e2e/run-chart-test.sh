8481507a29a1af4e122e44b00ada45840efd85f4
Marco Ebert <marco_ebert@icloud.com>
2026-02-04 12:41:18 +0100

    Tests: Bump Ginkgo to v2.28.1.

--- test/e2e/run-chart-test.sh
+++ test/e2e/run-chart-test.sh
@@ -78,7 +78,7 @@ fi
 
 if [ "${SKIP_IMAGE_CREATION:-false}" = "false" ]; then
   if ! command -v ginkgo &> /dev/null; then
-    go install github.com/onsi/ginkgo/v2/ginkgo@v2.27.5
+    go install github.com/onsi/ginkgo/v2/ginkgo@v2.28.1
   fi
   echo "[dev-env] building image"
   make -C ${DIR}/../../ clean-image build image
